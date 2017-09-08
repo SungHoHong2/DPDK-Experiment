eddabc477334cb91e2695a4173ccd05f5a77a52b
root <maverickjin88@gmail.com>
2017-09-08 14:44:49 -0600

    add

--- socket-experiment/basic/client.c
+++ socket-experiment/basic/client.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 #define PORT 8080
 
 int main(int argc, char const *argv[])
--- socket-experiment/basic/server.c
+++ socket-experiment/basic/server.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
+
+
 #define PORT 8080
 int main(int argc, char const *argv[])
 {
