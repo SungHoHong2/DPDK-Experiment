840e4ae69cac83bb34767b6655336667d2854bf1
SungHoHong2 <maverickjin88@gmail.com>
2017-12-11 17:33:29 -0700

    updated for DPDK-Experiments

---
+++ skeleton/main.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+#include <inttypes.h>
+#include <rte_eal.h>
+#include <rte_ethdev.h>
+#include <rte_cycles.h>
+#include <rte_lcore.h>
+#include <rte_mbuf.h>
+
+#define RX_RING_SIZE 128
+#define TX_RING_SIZE 512
+
+#define NUM_MBUFS 8191
+#define MBUF_CACHE_SIZE 250
+#define BURST_SIZE 32
+
+
+int main(int argc, char *argv[]){
+
+
+
+
+  return 0;
+
+}
