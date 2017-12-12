20be75065f08222f30248d7180a539866c58b6ba
SungHoHong2 <maverickjin88@gmail.com>
2017-12-12 12:34:34 -0700

    updated for DPDK-Experiments

--- skeleton/README.md
+++ skeleton/README.md
@@ -1,8 +1,5 @@
 ### FWD
 
-
 ```
-./build/basicfwd -l 0-3 -n 4 -- -q 8 -p 0x2 -l
-
-./build/basicfwd -l 1,2 -n 4 -- -p 0x3 --config="(0,0,1),(1,0,1)"
+./build/basicfwd -l 0-3 -n 4 -d librte_pmd_e1000.so -d librte_mempool_ring.so  -- -q 8 -p 0x3
 ```
--- skeleton/main.c
+++ skeleton/main.c
@@ -30,22 +30,24 @@ int main(int argc, char *argv[]){
 	argv += ret;
 
 
+  printf("number of ports: %u\n", nb_ports);
+
   /* Check that there is an even number of ports to send/receive on. */
 	nb_ports = rte_eth_dev_count(); // counting the number of available ports
 	if (nb_ports < 2 || (nb_ports & 1)){
-    printf("number of ports: %u\n", nb_ports);
   	rte_exit(EXIT_FAILURE, "Error: number of ports must be even\n");
   }
 
   /* Creates a new mempool in memory to hold the mbufs. */
 	mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports,
-		MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
+		MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id()); // this is the master core
 
-  // rte_socket_id
+  // rte_socket_id -> invoking here will get the master core
   // the ID of current lcoreid's physical socket
-  printf("current lcore's id %u\n", rte_socket_id());
-  printf("current lcore's id %u\n", rte_socket_id());
-  printf("current lcore's id %u\n", rte_socket_id());
+  // printf("current lcore's id %u\n", rte_socket_id());
+  // printf("current lcore's id %u\n", rte_socket_id());
+  // printf("current lcore's id %u\n", rte_socket_id());
+
 
 
 
