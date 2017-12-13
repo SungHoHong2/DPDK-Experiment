8c6bd9ca41762b8d37937bddf23486438b299f76
SungHoHong2 <maverickjin88@gmail.com>
2017-12-12 17:55:39 -0700

    updated for DPDK-Experiments

--- l2fwd/l2fwd_launch_one_lcore.h
+++ l2fwd/l2fwd_launch_one_lcore.h
@@ -1,11 +1,3 @@
-static int
-l2fwd_launch_one_lcore(__attribute__((unused)) void *dummy)
-{
-	l2fwd_main_loop();
-	return 0;
-}
-
-
 
 /* main processing loop */
 static void
@@ -14,4 +6,12 @@ l2fwd_main_loop(void)
 
   printf("hello");
 
+}
+
+
+static int
+l2fwd_launch_one_lcore(__attribute__((unused)) void *dummy)
+{
+	l2fwd_main_loop();
+	return 0;
 }
