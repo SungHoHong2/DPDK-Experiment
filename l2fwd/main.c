358e5316786d4f215e2ab61f7eb1cfab88262bf5
SungHoHong2 <maverickjin88@gmail.com>
2017-12-12 17:15:44 -0700

    updated for DPDK-Experiments

--- l2fwd/main.c
+++ l2fwd/main.c
@@ -73,6 +73,8 @@ struct lcore_queue_conf {
 
 struct lcore_queue_conf lcore_queue_conf[RTE_MAX_LCORE];
 
+static struct rte_eth_dev_tx_buffer *tx_buffer[RTE_MAX_ETHPORTS];
+
 
 int main(int argc, char **argv){
 
@@ -178,11 +180,55 @@ int main(int argc, char **argv){
     		qconf->n_rx_port++;
     		printf("Lcore %u: RX port %u\n", rx_lcore_id, (unsigned) portid);
     }
-
     nb_ports_available = nb_ports;
 
 
 
+  	/* Initialise each port */
+  	for (portid = 0; portid < nb_ports; portid++) {
+      /* skip ports that are not enabled */
+  		if ((l2fwd_enabled_port_mask & (1 << portid)) == 0) {
+  			printf("Skipping disabled port %u\n", (unsigned) portid);
+  			nb_ports_available--;
+  			continue;
+  		}
 
-  return 0;
+      /* init port */
+      printf("Initializing port %u... ", (unsigned) portid);
+      fflush(stdout);
+
+      ret = rte_eth_dev_configure(portid, 1, 1, &port_conf);
+      rte_eth_macaddr_get(portid,&l2fwd_ports_eth_addr[portid]);
+
+
+      /* init one RX queue */
+  		fflush(stdout);
+  		ret = rte_eth_rx_queue_setup(portid, 0, nb_rxd, rte_eth_dev_socket_id(portid), NULL, l2fwd_pktmbuf_pool);
+      /* init one TX queue on each port */
+  		fflush(stdout);
+  		ret = rte_eth_tx_queue_setup(portid, 0, nb_txd, rte_eth_dev_socket_id(portid), NULL);
+
+      /* Initialize TX buffers */
+  		tx_buffer[portid] = rte_zmalloc_socket("tx_buffer", RTE_ETH_TX_BUFFER_SIZE(MAX_PKT_BURST), 0, rte_eth_dev_socket_id(portid));
+
+      rte_eth_tx_buffer_init(tx_buffer[portid], MAX_PKT_BURST);
+
+      // ret = rte_eth_tx_buffer_set_err_callback(tx_buffer[portid], rte_eth_tx_buffer_count_callback, &port_statistics[portid].dropped);
+
+
+
+    }
+
+
+
+  for (portid = 0; portid < nb_ports; portid++) {
+		if ((l2fwd_enabled_port_mask & (1 << portid)) == 0)
+			continue;
+		printf("Closing port %d...", portid);
+		rte_eth_dev_stop(portid);
+		rte_eth_dev_close(portid);
+		printf(" Done\n");
+	}
+
+  return ret;
 }
