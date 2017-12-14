bcddb9a04f9b97c13f28822bf776783de76737e3
SungHoHong2 <maverickjin88@gmail.com>
2017-12-14 16:02:12 -0700

    updated for DPDK-Experiments

--- tcp-experiment/server.c
+++ tcp-experiment/server.c
@@ -120,10 +120,11 @@ int main(void){
 
     while(1) {  // main accept() loop
         inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
-        rx_throughput+=recv(new_fd, buf, MAXDATASIZE-1, 0);
+        r
+        rx_throughput+=sizeof(buf);
         //printf("server: received '%ld'\n",strlen(buf));
         send(new_fd, buf, MAXDATASIZE, 0);
-        tx_throughput+=strlen(buf);
+        tx_throughput+=sizeof(buf);
 
         if(++intervals==2000){
             /* Clear screen and move to top left */
