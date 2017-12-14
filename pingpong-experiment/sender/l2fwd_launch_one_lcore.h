2cbbdc81cc2639fd9610de6b9473e3868655eb66
SungHoHong2 <maverickjin88@gmail.com>
2017-12-13 19:01:39 -0700

    updated for DPDK-Experiments

--- pingpong-experiment/sender/l2fwd_launch_one_lcore.h
+++ pingpong-experiment/sender/l2fwd_launch_one_lcore.h
@@ -17,7 +17,7 @@ static void print_stats(void){
 		/* Clear screen and move to top left */
 	printf("%s%s", clr, topLeft);
 
-	printf("\nPort statistics ====================================");
+	printf("\nDPDK Pingpong Client ====================================");
 
 	for (portid = 0; portid < RTE_MAX_ETHPORTS; portid++) {
 		/* skip disabled ports */
