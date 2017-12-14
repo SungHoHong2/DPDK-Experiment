60dd3d63877ee09576441f07adfefa2964f67931
SungHoHong2 <maverickjin88@gmail.com>
2017-12-13 20:18:39 -0700

    updated for DPDK-Experiments

--- tcp-experiment/server.c
+++ tcp-experiment/server.c
@@ -108,34 +108,24 @@ int main(void)
 
 	while(1) {  // main accept() loop
 
-		printf("howdy before 1 \n");
-
 		sin_size = sizeof their_addr;
-
-		printf("howdy before 2 \n");
-
-		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-
-		printf("howdy before 3 \n");
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size); // there must be an while loop here
 
 		if (new_fd == -1) {
-
-			printf("howdy before 4 \n");
-
 			perror("accept");
 			continue;
 		}
-
-		printf("howdy after\n");
-
 
 		inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr),s, sizeof s);
 		printf("server: got connection from %s\n", s);
 
 		if (!fork()) { // this is the child process
 			close(sockfd); // child doesn't need the listener
-			if (send(new_fd, "Hello, world!", 13, 0) == -1)
-				perror("send");
+
+			for(int i=0; i<10; i++){
+					send(new_fd, "Hello, world!", 13, 0);
+			}
+
 			close(new_fd);
 			exit(0);
 		}
