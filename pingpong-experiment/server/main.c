7f7ca7ba6f36b8b515387a2415cbc72c45260c0a
SungHoHong2 <maverickjin88@gmail.com>
2017-12-14 19:52:50 -0700

    updated for DPDK-Experiments

--- pingpong-experiment/server/main.c
+++ pingpong-experiment/server/main.c
@@ -45,7 +45,7 @@
 // #define MAX_PKT_BURST 32
 #define MAX_PKT_BURST 32
 #define RTE_MBUF_SIZE 8000 			// RTE_MBUF_DEFAULT_BUF_SIZE
-#define PKT_SIZE 4086 					// sending packet size
+#define PKT_SIZE 2048 					// sending packet size
 // TUNABLES END
 
 
