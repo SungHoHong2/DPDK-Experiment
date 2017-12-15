aede94a7367f96154f2022d74bcf30aa9a4481dd
SungHoHong2 <maverickjin88@gmail.com>
2017-12-14 17:48:33 -0700

    updated for DPDK-Experiments

--- pingpong-experiment/client/l2fwd_launch_one_lcore.h
+++ pingpong-experiment/client/l2fwd_launch_one_lcore.h
@@ -132,7 +132,7 @@ static void l2fwd_main_loop(void){
           	nb_rx = rte_eth_rx_burst((uint8_t) portid, 0,
                  		pkts_burst, MAX_PKT_BURST);
 
-          	port_statistics[portid].rx += nb_rx * rte_pktmbuf_pkt_len(pkts_burst[j]);
+          	port_statistics[portid].rx += nb_rx;
 
 						for (j = 0; j < nb_rx; j++) {
 								rte_pktmbuf_free(pkts_burst[j]);
@@ -148,7 +148,7 @@ static void l2fwd_main_loop(void){
 						sent = rte_eth_tx_burst(portid, 0, rm, 1);
 
 						if (sent){
-							port_statistics[portid].tx += sent * rte_pktmbuf_pkt_len(rm[0]);
+							port_statistics[portid].tx += sent;
 						}
 
 						rte_pktmbuf_free(rm[0]);
