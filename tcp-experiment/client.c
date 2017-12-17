287258e0a3389752a64d4ac7c078920fa8bdee5e
SungHoHong2 <maverickjin88@gmail.com>
2017-12-17 14:35:10 -0700

    updated for DPDK-Experiments

--- tcp-experiment/client.c
+++ tcp-experiment/client.c
@@ -110,12 +110,13 @@ int main(){
       fclose(nic_file);
   }
 
-
+// 4946604
+//
   time (&start);
   while(1){
                 prev_latency = latency;
                 char send_data[PKT_SIZE];
-                memset( send_data, '*', (PKT_SIZE-2000) * sizeof(char));
+                memset( send_data, '*', (PKT_SIZE) * sizeof(char));
                 success=send(sockfd, send_data, PKT_SIZE, 0);
 
                 if(success && strlen(send_data)>0){
