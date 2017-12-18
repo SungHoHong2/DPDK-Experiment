c03ad41869ba0530129ad538686939630b184c9e
SungHoHong2 <maverickjin88@gmail.com>
2017-12-18 15:37:57 -0700

    updated for DPDK-Experiments

--- packet-experiment/custom_pktmbuf.h
+++ packet-experiment/custom_pktmbuf.h
@@ -71,6 +71,29 @@ static int custom_pktmbuf(void){
   }
 
 
+  // increase packet size to 2500
+  if(!rte_pktmbuf_append(m, 500)){
+      printf("append failed\n");
+  }else{
+      printf("length: %d\n", rte_pktmbuf_pkt_len(m));
+  }
+
+
+  // increase packet size to 2048
+  if(!rte_pktmbuf_append(m, 48)){
+      printf("append failed\n");
+  }else{
+      printf("length: %d\n", rte_pktmbuf_pkt_len(m));
+  }
+
+
+  // increase packet size to 2049
+  if(!rte_pktmbuf_append(m, 1)){
+      printf("append failed\n");
+  }else{
+      printf("length: %d\n", rte_pktmbuf_pkt_len(m));
+  }
+
 
 
   return 0;
