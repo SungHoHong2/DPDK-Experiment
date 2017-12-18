298e827ce59436bec12026c0780ad96b232e5812
SungHoHong2 <maverickjin88@gmail.com>
2017-12-18 14:03:10 -0700

    updated for DPDK-Experiments

--- packet-experiment/test_pktmbuf.h
+++ packet-experiment/test_pktmbuf.h
@@ -6,3 +6,22 @@ static int test_pktmbuf_with_non_ascii_data(void){
   memset(data, 0xff, rte_pktmbuf_pkt_len(m)); // update the data offset
   rte_pktmbuf_free(m);
 }
+
+
+static int test_one_pktmbuf(void){
+
+  struct rte_mbuf *m = NULL;
+	char *data, *data2, *hdr;
+	unsigned i;
+
+  m = rte_pktmbuf_alloc(pktmbuf_pool);
+
+  rte_pktmbuf_dump(stdout, m, 0);
+
+
+
+
+
+  return 0;
+
+}
