06e165638a246d925a663d3b2758382a754b0bc8
SungHoHong2 <maverickjin88@gmail.com>
2017-12-22 00:17:12 -0700

    updated for DPDK-Experiments

--- tcp-experiment/server.c
+++ tcp-experiment/server.c
@@ -19,7 +19,7 @@
 
 
 #define PORT "3490"  // the port users will be connecting to
-#define PKT_SIZE 500000 // max number of bytes we can get at once
+#define PKT_SIZE 1000000 // max number of bytes we can get at once
 #define BACKLOG 10     // how many pending connections queue will hold
 
 
