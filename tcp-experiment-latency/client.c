62cc785af37e3283e36a867f41de9ad9cde392c3
SungHoHong2 <maverickjin88@gmail.com>
2017-12-29 14:45:12 +0900

    updated for DPDK-Experiments

--- tcp-experiment-latency/client.c
+++ tcp-experiment-latency/client.c
@@ -52,6 +52,8 @@ int main(int argc , char *argv[])
             puts("Send failed");
             return 1;
         }
+        puts(message);
+
 
         //Receive a reply from the server
         if( recv(sock , server_reply , 2000 , 0) < 0)
