8268a35fc956461fb757d91518d1f02d5af71c2c
SungHoHong2 <maverickjin88@gmail.com>
2017-12-31 15:37:23 +0900

    updated for DPDK-Experiments

--- latency-experiment/f-stack-tcp/server.c
+++ latency-experiment/f-stack-tcp/server.c
@@ -117,7 +117,7 @@ int main(int argc, char * argv[])
     bzero(&my_addr, sizeof(my_addr));
     my_addr.sin_family = AF_INET;
     my_addr.sin_port = htons(80);
-    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);vi
+    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     int ret = ff_bind(sockfd, (struct linux_sockaddr *)&my_addr, sizeof(my_addr));
     if (ret < 0) {
