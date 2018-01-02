d2f504b46dc2551e111c3002037358c710df42cf
SungHoHong2 <maverickjin88@gmail.com>
2018-01-02 15:13:19 -0700

    updated for DPDK-Experiments

--- latency-experiment/f-stack-tcp/client.c
+++ latency-experiment/f-stack-tcp/client.c
@@ -1,7 +1,3 @@
-/*
-    C ECHO client example using sockets
-*/
-
 #include <unistd.h>
 #include <stdio.h> //printf
 #include <string.h>    //strlen
@@ -15,31 +11,41 @@
 
 int main(int argc , char *argv[])
 {
-    int sock;
+    int sockfd;
     struct sockaddr_in server;
     int total_length;
-    char start[64];
     struct timespec tps, tpe;
     long int latency;
 
+
     //Create socket
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1){
+    // sock = socket(AF_INET , SOCK_STREAM , 0);
+
+    ff_init(argc, argv);
+    sockfd = ff_socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd == -1){
         printf("Could not create socket");
     }
     puts("Socket created");
 
-    // server.sin_addr.s_addr = inet_addr("10.218.111.254");
-    server.sin_addr.s_addr = inet_addr("10.218.111.252");
 
+    bzero(&server, sizeof(server));
     server.sin_family = AF_INET;
     server.sin_port = htons( 80 );
+    server.sin_addr.s_addr = inet_addr("10.218.111.252"); // TCP
+    // server.sin_addr.s_addr = inet_addr("10.218.111.254"); // DPDK
+
 
     //Connect to remote server
-    // int ff_connect(int s, const struct linux_sockaddr *name, socklen_t namelen);
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0){
-        perror("connect failed. Error");
-        return 1;
+    // if (connect(sockfd , (struct sockaddr *)&server , sizeof(server)) < 0){
+    //     perror("connect failed. Error");
+    //     return 1;
+    // }
+
+    if(ff_connect(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0 ){
+      perror("connect failed. Error");
+      return 1;
     }
 
     printf("PKT_SIZE: %d\n", PKT_SIZE);
@@ -47,54 +53,53 @@ int main(int argc , char *argv[])
     total_length = latency = 0;
     clock_gettime(CLOCK_REALTIME, &tps);
 
-    //keep communicating with server
-    for(int i =0; i<TOTAL_SEND*2; i++)
-    {
-        char message[PKT_SIZE] , server_reply[PKT_SIZE];
+    // //keep communicating with server
+    // for(int i =0; i<TOTAL_SEND*2; i++)
+    // {
+    //     char message[PKT_SIZE] , server_reply[PKT_SIZE];
+    //
+    //     if(i%2==0){
+    //         memset( message, '*', sizeof(char)*PKT_SIZE);
+    //
+    //         //Send some data
+    //         if( send(sock , message , strlen(message) , 0) < 0){
+    //             puts("Send failed");
+    //             return 1;
+    //         }
+    //         // usleep(1);
+    //
+    //     } else {
+    //         //Receive a reply from the server
+    //         if( recv(sock , server_reply , PKT_SIZE, 0) < 0){
+    //             puts("recv failed");
+    //             break;
+    //         }
+    //
+    //         total_length += strlen(server_reply);
+    //         // usleep(1);
+    //     }
+    //
+    //     // puts(server_reply);
+    // }
+    //
+    // clock_gettime(CLOCK_REALTIME, &tpe);
+    //
+    // if(total_length == TOTAL_SEND * PKT_SIZE)
+    //   printf("identical total_length: %d\n", total_length);
+    // else{
+    //   printf("expected_length: %d\n", TOTAL_SEND * PKT_SIZE);
+    //   printf("actual_length: %d\n", total_length);
+    //   printf("unidentical total_length: %d\n", total_length);
+    // }
+    //
+    // latency =  (tpe.tv_sec - tps.tv_sec) * 1000000000;
+    // latency += (tpe.tv_nsec - tps.tv_nsec);
+    // // latency =  tpe.tv_nsec - tps.tv_nsec;
+    // latency /= TOTAL_SEND;
+    //
+    // printf("latency: %ld\n", latency);
+    //
 
-        if(i%2==0){
-            memset( message, '*', sizeof(char)*PKT_SIZE);
-
-            //Send some data
-            if( send(sock , message , strlen(message) , 0) < 0){
-                puts("Send failed");
-                return 1;
-            }
-            // usleep(1);
-
-        } else {
-            //Receive a reply from the server
-            if( recv(sock , server_reply , PKT_SIZE, 0) < 0){
-                puts("recv failed");
-                break;
-            }
-
-            total_length += strlen(server_reply);
-            // usleep(1);
-        }
-
-        // puts(server_reply);
-    }
-
-    clock_gettime(CLOCK_REALTIME, &tpe);
-
-    if(total_length == TOTAL_SEND * PKT_SIZE)
-      printf("identical total_length: %d\n", total_length);
-    else{
-      printf("expected_length: %d\n", TOTAL_SEND * PKT_SIZE);
-      printf("actual_length: %d\n", total_length);
-      printf("unidentical total_length: %d\n", total_length);
-    }
-
-    latency =  (tpe.tv_sec - tps.tv_sec) * 1000000000;
-    latency += (tpe.tv_nsec - tps.tv_nsec);
-    // latency =  tpe.tv_nsec - tps.tv_nsec;
-    latency /= TOTAL_SEND;
-
-    printf("latency: %ld\n", latency);
-
-
-
-    close(sock);
+    ff_close(sockfd);
     return 0;
 }
--- latency-experiment/f-stack-tcp/fstack-server.c
+++ latency-experiment/f-stack-tcp/fstack-server.c
@@ -118,4 +118,66 @@ int main(int argc, char * argv[])
 
     ff_run(loop, NULL);
     return 0;
+
+
+    // int socket_desc , client_sock , c , read_size;
+    // struct sockaddr_in server , client;
+    // char client_message[PKT_SIZE];
+    //
+    // //Create socket
+    // socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    // if (socket_desc == -1)
+    // {
+    //     printf("Could not create socket");
+    // }
+    // puts("Socket created");
+    //
+    // //Prepare the sockaddr_in structure
+    // server.sin_family = AF_INET;
+    // server.sin_addr.s_addr = INADDR_ANY;
+    // server.sin_port = htons( 80 );
+    //
+    // //Bind
+    // if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    // {
+    //     //print the error message
+    //     perror("bind failed. Error");
+    //     return 1;
+    // }
+    // puts("bind done");
+    //
+    // //Listen
+    // listen(socket_desc , 3);
+    //
+    // //Accept and incoming connection
+    // puts("Waiting for incoming connections...");
+    // c = sizeof(struct sockaddr_in);
+    //
+    // //accept connection from an incoming client
+    // client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+    // if (client_sock < 0)
+    // {
+    //     perror("accept failed");
+    //     return 1;
+    // }
+    // printf("PKT_SIZE: %d\n", PKT_SIZE);
+    //
+    // //Receive a message from client
+    // while( (read_size = recv(client_sock , client_message , PKT_SIZE , 0)) > 0 )
+    // {
+    //     //Send the message back to client
+    //     write(client_sock , client_message , PKT_SIZE);
+    // }
+    //
+    // if(read_size == 0)
+    // {
+    //     puts("Client disconnected");
+    //     fflush(stdout);
+    // }
+    // else if(read_size == -1)
+    // {
+    //     perror("recv failed");
+    // }
+
+
 }
