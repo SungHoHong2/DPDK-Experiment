05cbad8cf5223fe818a72ddb7a27139d6758ba21
SungHoHong2 <maverickjin88@gmail.com>
2018-01-02 13:01:39 -0700

    updated for DPDK-Experiments

--- latency-experiment/f-stack-tcp/client.c
+++ latency-experiment/f-stack-tcp/client.c
@@ -9,13 +9,14 @@
 #include <arpa/inet.h> //inet_addr
 
 #define TOTAL_SEND 10000
+#define PKT_SIZE 64
 
 int main(int argc , char *argv[])
 {
     int sock;
     struct sockaddr_in server;
     int total_length;
-    char start[64];
+    // char start[64];
 
     //Create socket
     sock = socket(AF_INET , SOCK_STREAM , 0);
@@ -40,50 +41,12 @@ int main(int argc , char *argv[])
 
     total_length = 0;
     //keep communicating with server
-    // for(int i =0; i<TOTAL_SEND*2; i++)
-    // {
-    //     char message[PKT_SIZE] , server_reply[PKT_SIZE];
-    //
-    //     if(i%2==0){
-    //         memset( message, '*', sizeof(char)*PKT_SIZE);
-    //
-    //         //Send some data
-    //         if( send(sock , message , strlen(message) , 0) < 0){
-    //             puts("Send failed");
-    //             return 1;
-    //         }
-    //         usleep(1);
-    //
-    //     } else {
-    //         //Receive a reply from the server
-    //         if( recv(sock , server_reply , PKT_SIZE , 0) < 0){
-    //             puts("recv failed");
-    //             break;
-    //         }
-    //
-    //         total_length += strlen(server_reply);
-    //         usleep(1);
-    //     }
-    //
-    //
-    //
-    //
-    //     // puts("Server reply :");
-    //     // puts(server_reply);
-    // }
 
-
-    int pkt_size;
-    int recv_rtn;
-    while(1)
+    for(int i =0; total_length<TOTAL_SEND*PKT_SIZE; i++)
     {
+        char message[PKT_SIZE] , server_reply[PKT_SIZE];
 
-        printf("total size of the packet: ");
-        scanf("%s", start);
-        pkt_size = atoi(start);
-        char message[pkt_size] , server_reply[pkt_size];
-
-            memset( message, '*', sizeof(char)*pkt_size);
+            memset( message, '*', sizeof(char)*PKT_SIZE);
 
             //Send some data
             if( send(sock , message , strlen(message) , 0) < 0){
@@ -92,16 +55,50 @@ int main(int argc , char *argv[])
             }
 
             //Receive a reply from the server
-
-            int recv_rtn = recv(sock , server_reply , pkt_size , 0);
-            if( recv_rtn < 0){
+            if( recv(sock , server_reply , PKT_SIZE , 0) < 0){
                 puts("recv failed");
                 break;
             }
-            total_length = strlen(server_reply);
-            printf("total_length: %d\n", recv_rtn);
+
+            total_length += strlen(server_reply);
+
+        // puts("Server reply :");
+        // puts(server_reply);
     }
 
+
+
+
+
+    // int pkt_size;
+    // int recv_rtn;
+    // while(1)
+    // {
+    //
+    //     printf("total size of the packet: ");
+    //     scanf("%s", start);
+    //     pkt_size = atoi(start);
+    //     char message[pkt_size] , server_reply[pkt_size];
+    //
+    //         memset( message, '*', sizeof(char)*pkt_size);
+    //
+    //         //Send some data
+    //         if( send(sock , message , strlen(message) , 0) < 0){
+    //             puts("Send failed");
+    //             return 1;
+    //         }
+    //
+    //         //Receive a reply from the server
+    //
+    //         int recv_rtn = recv(sock , server_reply , pkt_size , 0);
+    //         if( recv_rtn < 0){
+    //             puts("recv failed");
+    //             break;
+    //         }
+    //         total_length = strlen(server_reply);
+    //         printf("total_length: %d\n", recv_rtn);
+    // }
+    //
 
     close(sock);
     return 0;
