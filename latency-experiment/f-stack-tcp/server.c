45edb19fd8056013c1efe0a60dca7c8464e79fd8
SungHoHong2 <maverickjin88@gmail.com>
2018-01-02 11:25:31 -0700

    updated for DPDK-Experiments

--- latency-experiment/f-stack-tcp/server.c
+++ latency-experiment/f-stack-tcp/server.c
@@ -68,7 +68,7 @@ int loop(void *arg)
         } else if (event.filter == EVFILT_READ) { // this will work
             size_t readlen = ff_read(clientfd, buf, sizeof(buf));
             ff_write(clientfd, buf, sizeof(buf));
-            memset( buf, '\0', sizeof(char));
+            // memset( buf, '\0', sizeof(char));
         } else {  // or this one will work
             printf("unknown event: %8.8X\n", event.flags);
         }
