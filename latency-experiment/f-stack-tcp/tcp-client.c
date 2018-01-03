b9b9a0feae2289e62b41ffce79bdc9e7dd13a780
SungHoHong2 <maverickjin88@gmail.com>
2018-01-03 15:19:17 -0700

    updated for DPDK-Experiments

--- latency-experiment/f-stack-tcp/tcp-client.c
+++ latency-experiment/f-stack-tcp/tcp-client.c
@@ -1,45 +1,116 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// system types
+#include <sys/types.h>
+// socket API
 #include <sys/socket.h>
+// struct sockaddr_in
+#include <netinet/in.h>
+// inet_ntoa()
 #include <arpa/inet.h>
+// system call
+#include <unistd.h>
+// struct hostent and gethostbyname()
+#include <netdb.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <errno.h>
-#include <sys/ioctl.h>
+extern int errno;
+#define BUFFSIZE 128
+#define PORT 10012
 
-int sockfd = -1;
-int check_connection = 0;
+// Print error information
+int errexit(const char* format, ...);
+// Print work information
+int echo(const char* format, ...);
 
-
-int loop(void *arg) {
-    struct sockaddr_in my_addr;
-    bzero(&my_addr, sizeof(my_addr));
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(80);
-    // my_addr.sin_port = htons(22);
-    my_addr.sin_addr.s_addr = inet_addr("10.218.111.254");
-    // my_addr.sin_addr.s_addr = inet_addr("10.218.111.252");
-
-    int on = 1;
-    ff_ioctl(sockfd, FIONBIO, &on);
-    if (ff_connect(sockfd, (struct linux_sockaddr *)&my_addr, sizeof(my_addr)) == -1) {
-        printf("ff_connect failed %d: %s\n", errno, strerror(errno));
-    } else {
-        printf("ff_connect OK\n");
-        while(1){}
-    }
-
+char* getMessage(char* buffer, int len, FILE* fp)
+{
+    echo(">> Input message: ");
+    return fgets(buffer, len, fp);
 }
 
-int main(int argc, char * argv[]) {
-    ff_init(argc, argv);
-
-    sockfd = ff_socket(AF_INET, SOCK_STREAM, 0);
-    printf("sockfd:%d\n", sockfd);
-    if (sockfd < 0) {
-        printf("ff_socket failed\n");
-        exit(1);
+int recvFromServer(int sockfd, char* buf, int buf_size)
+{
+    int n, offset = 0;
+    errno = 0;
+    while (buf_size - offset > 0 &&
+            (n = recv(sockfd, buf + offset, buf_size - offset, MSG_DONTWAIT)) > 0)
+    {
+        offset += n;
     }
+    if (offset == 0 && errno == EAGAIN)
+    {
+        echo("[CLIENT] no message received.\n");
+        return -1;
+    }
+    else
+    {
+        return offset;
+    }
+}
 
-    ff_run(loop, NULL);
-    return 0;
+// Handle socket session
+void process(FILE *fp, int sockfd)
+{
+    char sendline[BUFFSIZE], recvline[BUFFSIZE];
+    int numbytes;
+    while (getMessage(sendline, BUFFSIZE, fp) != NULL)
+    {
+        send(sockfd, sendline, strlen(sendline), 0);
+        sleep(1);
+        if ((numbytes = recvFromServer(sockfd, recvline, BUFFSIZE)) == 0)
+        {
+            echo("[CLIENT] server terminated.\n");
+            return;
+        }
+        else if (numbytes > 0)
+        {
+            recvline[numbytes] = '\0';
+            echo("Received: %s\n", recvline);
+        }
+    }
+    echo("[CLIENT] exit.\n");
+}
+
+// Main function
+int main(int argc,char* argv[])
+{
+    int sock;
+    struct hostent *hent;
+    struct sockaddr_in server;
+    char *host = "127.0.0.1";
+    unsigned short port = PORT;
+    switch (argc)
+    {
+        case 1:
+            break;
+        case 2:
+            host = argv[1];
+            break;
+        case 3:
+            host = argv[1];
+            port = atoi(argv[2]);
+            break;
+        default:
+            errexit("usage: %s [host [port]]\n", argv[0]);
+    }
+    // convert decimal IP to binary IP
+    if ((hent = gethostbyname(host)) == NULL)
+        errexit("gethostbyname failed.\n");
+    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+        errexit("create socket failed: %s\n", strerror(errno));
+
+    bzero(&server, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr = *((struct in_addr*)hent->h_addr);
+    echo("[CLIENT] server addr: %s, port: %u\n", inet_ntoa(server.sin_addr), ntohs(server.sin_port));
+
+    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0)
+        errexit("connect to server failed: %s\n", strerror(errno));
+
+    echo("[CLIENT] connected to server %s\n", inet_ntoa(server.sin_addr));
+    // Send request to server
+    process(stdin, sock);
+    close(sock);
 }
