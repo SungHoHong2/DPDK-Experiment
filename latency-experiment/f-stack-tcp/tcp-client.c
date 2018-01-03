4847c334a4390dfe7fbaa1756cbb5c88bfd5ad02
SungHoHong2 <maverickjin88@gmail.com>
2018-01-03 15:39:21 -0700

    updated for DPDK-Experiments

--- latency-experiment/f-stack-tcp/tcp-client.c
+++ latency-experiment/f-stack-tcp/tcp-client.c
@@ -1,102 +1,120 @@
-// system types
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <strings.h>
 #include <sys/types.h>
-// socket API
 #include <sys/socket.h>
-// struct sockaddr_in
-#include <netinet/in.h>
-// inet_ntoa()
 #include <arpa/inet.h>
-// system call
-#include <unistd.h>
+#include <errno.h>
+#include <assert.h>
+#include <sys/ioctl.h>
+
+
 // struct hostent and gethostbyname()
 #include <netdb.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-extern int errno;
-#define BUFFSIZE 128
-#define PORT 80
+
+// sleep function
+#include <unistd.h>
+
+
+#define PKT_SIZE 64
+#define MAX_EVENTS 512
+
+#define MAX_EVENTS 512
+#define BUFSIZE 10
+
+struct epoll_event ev;
+struct epoll_event events[MAX_EVENTS];
+
+int epfd;
+int sockfd;
+char hello[PKT_SIZE];
+char buffer[PKT_SIZE];
+int status = 0;
+int succ = 0;
 
 
 
-char* getMessage(char* buffer, int len, FILE* fp)
-{
-    printf(">> Input message: ");
-    return fgets(buffer, len, fp);
-}
+int main(int argc,char* argv[]){
 
-int recvFromServer(int sockfd, char* buf, int buf_size)
-{
-    int n, offset = 0;
-    errno = 0;
-    while (buf_size - offset > 0 &&
-            (n = recv(sockfd, buf + offset, buf_size - offset, MSG_DONTWAIT)) > 0)
-    {
-        offset += n;
-    }
-    if (offset == 0 && errno == EAGAIN)
-    {
-        printf("[CLIENT] no message received.\n");
-        return -1;
-    }
-    else
-    {
-        return offset;
-    }
-}
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  printf("sockfd: %d\n", sockfd);
 
-// Handle socket session
-void process(FILE *fp, int sockfd)
-{
-    char sendline[BUFFSIZE], recvline[BUFFSIZE];
-    int numbytes;
-    while (getMessage(sendline, BUFFSIZE, fp) != NULL)
-    {
-        send(sockfd, sendline, strlen(sendline), 0);
-        sleep(1);
-        if ((numbytes = recvFromServer(sockfd, recvline, BUFFSIZE)) == 0)
-        {
-            printf("[CLIENT] server terminated.\n");
-            return;
+  if (sockfd < 0) {
+      printf("socket failed\n");
+      exit(1);
+  }
+
+
+  memset( hello, '*', PKT_SIZE * sizeof(char));
+  struct sockaddr_in serv_addr;
+  bzero(&serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(80);
+  serv_addr.sin_addr.s_addr = inet_addr("10.218.111.254");
+
+  assert((epfd = epoll_create(0)) > 0);
+  ev.data.fd = sockfd;
+  ev.events = EPOLLOUT;
+  epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev);
+  fprintf(stderr, "epoll fd: %d\n", epfd);
+
+  printf("connecting...\n");
+  int ret = connect(sockfd, (struct linux_sockaddr *)&serv_addr, sizeof(serv_addr));
+  if (ret < 0) {
+      perror("connect");
+  }
+
+
+  while(1){
+
+        int nevents = epoll_wait(epfd, events, MAX_EVENTS, 0);
+        struct epoll_event event;
+        for (int i = 0; i < nevents; ++i) {
+            if(events[i].events & EPOLLOUT) {
+                if (status++ == 0)
+                    printf("connection establised, fd %d\n", events[i].data.fd);
+                else
+                    printf("epoll %d times, fd %d\n", status, events[i].data.fd);
+
+
+                int n = strlen(hello);
+                int nsend = write(events[i].data.fd, hello, PKT_SIZE);
+                if(nsend < 0 && errno != EAGAIN) {
+                    perror("send error");
+                    close(events[i].data.fd);
+                    exit(1);
+                }
+                // printf("message delivered!\n");
+                ev.data.fd = sockfd;
+                ev.events = EPOLLIN;
+                assert(epoll_ctl(epfd, EPOLL_CTL_MOD, sockfd, &ev)==0);
+                // printf("events modified!\n");
+            }
+            if(events[i].events & EPOLLIN) {
+                // printf("sockfd: %d\n", sockfd);
+                // printf("receiving data... fd %d\n", events[i].data.fd);
+                // printf("read success %d times\n", succ);
+
+                struct epoll_event ev;
+                ev.data.fd = sockfd;
+                ev.events = EPOLLOUT;
+
+                epoll_ctl(epfd, EPOLL_CTL_MOD, sockfd, &ev);
+
+                int nrecv = recv(events[i].data.fd, buffer, PKT_SIZE, 0) ;
+                if(nrecv == -1 && errno != EAGAIN)
+                    perror("read error");
+                if((nrecv == -1 && errno == EAGAIN) || nrecv == 0)
+                    break;
+                if (nrecv > 0) succ++;
+
+                printf("stringlength: %ld\n", strlen(buffer));
+                exit(1);
+            }
         }
-        else if (numbytes > 0)
-        {
-            recvline[numbytes] = '\0';
-            printf("Received: %s\n", recvline);
-        }
-    }
-    printf("[CLIENT] exit.\n");
-}
+  }
 
-// Main function
-int main(int argc,char* argv[])
-{
-    int sock;
-    struct hostent *hent;
-    struct sockaddr_in server;
-    char *host = "10.218.111.252";
-    unsigned short port = PORT;
-
-    // convert decimal IP to binary IP
-    if ((hent = gethostbyname(host)) == NULL)
-        printf("gethostbyname failed.\n");
-    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-        printf("create socket failed: %s\n", strerror(errno));
-
-    bzero(&server, sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    server.sin_addr = *((struct in_addr*)hent->h_addr);
-    printf("[CLIENT] server addr: %s, port: %u\n", inet_ntoa(server.sin_addr), ntohs(server.sin_port));
-
-    if (connect(sock, (struct sockaddr*)&server, sizeof(server)) < 0){
-        printf("connect to server failed: %s\n", strerror(errno));
-        return 0;
-      }
-
-    printf("[CLIENT] connected to server %s\n", inet_ntoa(server.sin_addr));
-    // Send request to server
-    process(stdin, sock);
-    close(sock);
+return 0;
 }
