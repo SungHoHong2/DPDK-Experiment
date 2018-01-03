fee7beef136d13c60e60a49c18eaefafcd4b7e1f
SungHoHong2 <maverickjin88@gmail.com>
2018-01-03 14:15:18 -0700

    updated for DPDK-Experiments

--- latency-experiment/f-stack-tcp/tcp-server.c
+++ latency-experiment/f-stack-tcp/tcp-server.c
@@ -85,7 +85,7 @@ int main(int argc, char * argv[])
     my_addr.sin_port = htons(80);
     my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    int ret = bind(sockfd, (struct linux_sockaddr *)&my_addr, sizeof(my_addr));
+    int ret = bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr));
     if (ret < 0) {
         printf("ff_bind failed\n");
         exit(1);
