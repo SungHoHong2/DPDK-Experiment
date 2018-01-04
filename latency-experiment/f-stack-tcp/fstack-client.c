8fe4399d6bfe65d1d7cde0501cb025135a7d5f5e
SungHoHong2 <maverickjin88@gmail.com>
2018-01-03 17:06:42 -0700

    updated for DPDK-Experiments

--- latency-experiment/f-stack-tcp/fstack-client.c
+++ latency-experiment/f-stack-tcp/fstack-client.c
@@ -86,7 +86,7 @@ int loop(void *arg) {
             // printf("stringlength: %ld\n", strlen(buffer));
 
             if(curr_bytes>=limit_bytes){
-                  break;
+              perror("raise() failed");
             }
         }
     }
