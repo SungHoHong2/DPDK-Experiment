821ca22947eafe3158a06610728971cd1d2dd50e
SungHoHong2 <maverickjin88@gmail.com>
2018-01-05 22:04:06 -0700

    updated for DPDK-Experiments

--- netperf-experiment/netperf/src/netserver.c
+++ netperf-experiment/netperf/src/netserver.c
@@ -803,7 +803,8 @@ spawn_child() {
 
 
 SOCKET epfd;
-
+struct epoll_event ev;
+struct epoll_event events[MAX_EVENTS];
 
 void
 accept_connection(SOCKET listen_fd) {
@@ -815,7 +816,7 @@ accept_connection(SOCKET listen_fd) {
 #endif
 
   printf("server is running...\n");
-  assert((epfd = ff_epoll_create(1)) > 0);
+  epfd = ff_epoll_create(0);
   ev.data.fd = temp_socket;
   ev.events = EPOLLIN;
   ff_epoll_ctl(epfd, EPOLL_CTL_ADD, temp_socket, &ev);
