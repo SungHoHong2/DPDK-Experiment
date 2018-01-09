a512852a0a89e84086c7fadf002974d83bdd14d3
SungHoHong2 <maverickjin88@gmail.com>
2018-01-09 15:55:03 -0700

    updated for DPDK-Experiments

---
+++ seastar-experiment/e03_continuations.cc
@@ -0,0 +1,34 @@
+#include "core/app-template.hh"
+#include "core/reactor.hh"
+#include <iostream>
+#include <stdexcept>
+#include "core/sleep.hh"
+#include <iostream>
+
+
+seastar::future<int> incr(int i) {
+    using namespace std::chrono_literals;
+    return seastar::sleep(10ms).then([i] { return i + 1; });
+}
+
+
+seastar::future<> f() {
+    return incr(3).then([] (int val) {
+        std::cout << "Got " << val << "\n";
+    });
+}
+
+
+int main(int argc, char** argv) {
+    seastar::app_template app;
+    try {
+        app.run(argc, argv, f);
+
+
+    } catch(...) {
+        cerr << "Failed to start application: "
+                  << std::current_exception() << "\n";
+        return 1;
+    }
+    return 0;
+}
