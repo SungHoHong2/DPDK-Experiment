874fdd2a07c7998896c67c09b81df618b6f331a1
SungHoHong2 <maverickjin88@gmail.com>
2018-01-09 13:45:22 -0700

    updated for DPDK-Experiments

--- seastar-experiment/lambda/tutorial_1.cpp
+++ seastar-experiment/lambda/tutorial_1.cpp
@@ -19,5 +19,19 @@ int main(){
   auto func = [](int a, int b) -> int { return a+b; };
   cout << func(2, 3) << endl;
 
+  int n=10;
+  vector<int> v(n);
+
+  for(int i = n - 1, j = 0; i >= 0; i--, j++) v[j] = i + 1;
+
+  for(int i = 0; i < n; i++) cout << v[i] << " "; cout << endl;
+
+  // sort the vector
+  sort(v.begin(),v.end(),[](int i, int j) -> bool{ return (i < j);});
+
+	for(int i = 0; i < n; i++) cout << v[i] << " "; cout << endl;
+
+
+
   return 0;
 }
