a302e3f219425e106344b4d354f11199ca7b2fe8
SungHoHong2 <maverickjin88@gmail.com>
2018-01-09 15:24:45 -0700

    updated for DPDK-Experiments

--- seastar-experiment/e02_futures_continuation.cc
+++ seastar-experiment/e02_futures_continuation.cc
@@ -1,14 +1,16 @@
 #include "core/app-template.hh"
-#include "core/sleep.hh"
+#include "util/log.hh"
 #include <iostream>
-
-int main(int argc, char** argv){
+#include <stdexcept>
+extern seastar::future<> f();
+int main(int argc, char** argv) {
     seastar::app_template app;
-    app.run(argc, argv, [] {
-          std::cout << "Sleeping... " << std::flush;
-          using namespace std::chrono_literals;
-          return seastar::sleep(1s).then([] {
-                std::cout << "Done.\n";
-          });
-    });
+    try {
+        app.run(argc, argv, f);
+    } catch(...) {
+        std::cerr << "Couldn't start application: "
+                  << std::current_exception() << "\n";
+        return 1;
+    }
+    return 0;
 }
