d87da326f3c8fe274593c96f3f3eea3983bff965
SungHoHong2 <maverickjin88@gmail.com>
2018-01-09 13:24:23 -0700

    updated for DPDK-Experiments

--- seastar-experiment/lambda/tutorial_1.cpp
+++ seastar-experiment/lambda/tutorial_1.cpp
@@ -5,11 +5,15 @@ using namespace std;
 
 int main(){
 
-  // return a number
+// return a number
   int result = []()->int {return 4;}();
   cout << result << endl;
 
+// []( input_paramter_declaration )->returned_type { body_of_the_lambda_expression }( parameters )
   result = [](int input)-> int {return  2 * input;}(10);
+  cout << result << endl;
+
+  result = [](int a, int b)-> int {return a + b;}(2,4);
   cout << result << endl;
 
   return 0;
