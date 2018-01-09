45382f37191dd588bc3ad0cb3264fd2b5c43a694
SungHoHong2 <maverickjin88@gmail.com>
2018-01-09 15:21:10 -0700

    updated for DPDK-Experiments

--- seastar-experiment/e02_futures_continuation.cc
+++ seastar-experiment/e02_futures_continuation.cc
@@ -1,14 +1,14 @@
 #include "core/app-template.hh"
-#include "core/reactor.hh"
+#include "core/sleep.hh"
 #include <iostream>
-int main(int argc, char** argv) {
+
+int main(int argc, char** argv){
     seastar::app_template app;
-
-        app.run(argc, argv, [] {
-                std::cout << seastar::smp::count << "\n"; // the number of started threads:
-                return seastar::make_ready_future<>();
-        }); // return 8
+    app.run(argc, argv, [] {
+          std::cout << "Sleeping... " << std::flush;
+          using namespace std::chrono_literals;
+          return seastar::sleep(1s).then([] {
+                std::cout << "Done.\n";
+          });
+    });
 }
-
-// can change the allocation of cores by using argument
-// ./e01_threads_memory -c3
