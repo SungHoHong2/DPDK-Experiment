7d69cee06d4a167d2a3d3720421cfa645c20669d
SungHoHong2 <maverickjin88@gmail.com>
2018-01-12 13:58:55 -0700

    updated for DPDK-Experiments

--- seastar-experiment/seastar-server/server.cc
+++ seastar-experiment/seastar-server/server.cc
@@ -1,46 +1,43 @@
-#include "core/app-template.hh"
-#include "util/log.hh"
-#include "core/sleep.hh"
+
+#include <cmath>
 #include "core/reactor.hh"
-#include <iostream>
-#include <stdexcept>
-#include <iostream>
+#include "core/app-template.hh"
+#include "core/sleep.hh"
+#include "net/dns.hh"
+#include "tls_echo_server.hh"
 
-seastar::future<> service_loop();
-
-seastar::future<> f() {
-  return seastar::parallel_for_each(boost::irange<unsigned>(0, seastar::smp::count),
-           [] (unsigned c) {
-       return seastar::smp::submit_to(c, service_loop);
-   });
-}
+using namespace seastar;
+namespace bpo = boost::program_options;
 
 
-int main(int argc, char** argv) {
-    seastar::app_template app;
-    try {
-        app.run(argc, argv, f);
-    } catch(...) {
-        std::cerr << "Failed to start application: "
-                  << std::current_exception() << "\n";
-        return 1;
-    }
-    return 0;
-}
+int main(int ac, char** av) {
+    app_template app;
+    app.add_options()
+                    ("port", bpo::value<uint16_t>()->default_value(10000), "Server port")
+                    ("verbose,v", bpo::value<bool>()->default_value(false)->implicit_value(true), "Verbose")
+                    ;
+    return app.run_deprecated(ac, av, [&] {
+        auto&& config = app.configuration();
+        uint16_t port = config["port"].as<uint16_t>();
+        auto addr = config["address"].as<std::string>();
+        auto verbose = config["verbose"].as<bool>();
 
+        std::cout << "Starting..." << std::endl;
+        return net::dns::resolve_name(addr).then([=](net::inet_address a) {
+            ipv4_addr ia(a, port);
 
-seastar::future<> service_loop() {
-    seastar::socket_address sa;
-    sa = seastar::make_ipv4_address({1234});
-
-    return seastar::do_with(seastar::listen(sa),
-            [] (auto& listener) {
-        return seastar::keep_doing([&listener] () {
-            return listener.accept().then(
-                [] (seastar::connected_socket s, seastar::socket_address a) {
-
-                    std::cout << "Accepted connection from " << a << "\n";
+            auto server = ::make_shared<seastar::sharded<echoserver>>();
+            return server->start(verbose).then([=]() {
+                return server->invoke_on_all(&echoserver::listen, socket_address(ia), tls::client_auth::NONE);
+            }).handle_exception([=](auto e) {
+                std::cerr << "Error: " << e << std::endl;
+                engine().exit(1);
+            }).then([=] {
+                std::cout << "TLS echo server running at " << addr << ":" << port << std::endl;
+                engine().at_exit([server] {
+                    return server->stop();
                 });
+            });
         });
     });
 }
