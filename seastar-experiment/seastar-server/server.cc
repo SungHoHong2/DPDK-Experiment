60f99f1194fd268289918b54b606634ff08f42c7
SungHoHong2 <maverickjin88@gmail.com>
2018-01-12 16:10:08 -0700

    updated for DPDK-Experiments

--- seastar-experiment/seastar-server/server.cc
+++ seastar-experiment/seastar-server/server.cc
@@ -31,6 +31,39 @@ namespace bpo = boost::program_options;
 using namespace seastar;
 using namespace httpd;
 
+char html[] =
+"HTTP/1.1 200 OK\r\n"
+"Server: F-Stack\r\n"
+"Date: Sat, 25 Feb 2017 09:26:33 GMT\r\n"
+"Content-Type: text/html\r\n"
+"Content-Length: 439\r\n"
+"Last-Modified: Tue, 21 Feb 2017 09:44:03 GMT\r\n"
+"Connection: keep-alive\r\n"
+"Accept-Ranges: bytes\r\n"
+"\r\n"
+"<!DOCTYPE html>\r\n"
+"<html>\r\n"
+"<head>\r\n"
+"<title>Welcome to F-Stack!</title>\r\n"
+"<style>\r\n"
+"    body {  \r\n"
+"        width: 35em;\r\n"
+"        margin: 0 auto; \r\n"
+"        font-family: Tahoma, Verdana, Arial, sans-serif;\r\n"
+"    }\r\n"
+"</style>\r\n"
+"</head>\r\n"
+"<body>\r\n"
+"<h1>Welcome to F-Stack!</h1>\r\n"
+"\r\n"
+"<p>For online documentation and support please refer to\r\n"
+"<a href=\"http://F-Stack.org/\">F-Stack.org</a>.<br/>\r\n"
+"\r\n"
+"<p><em>Thank you for using F-Stack.</em></p>\r\n"
+"</body>\r\n"
+"</html>";
+
+
 class handl : public httpd::handler_base {
 public:
     virtual future<std::unique_ptr<reply> > handle(const sstring& path,
@@ -43,36 +76,7 @@ public:
 
 void set_routes(routes& r) {
     function_handler* h1 = new function_handler([](const_req req) {
-        return "HTTP/1.1 200 OK\r\n"
-        "Server: F-Stack\r\n"
-        "Date: Sat, 25 Feb 2017 09:26:33 GMT\r\n"
-        "Content-Type: text/html\r\n"
-        "Content-Length: 439\r\n"
-        "Last-Modified: Tue, 21 Feb 2017 09:44:03 GMT\r\n"
-        "Connection: keep-alive\r\n"
-        "Accept-Ranges: bytes\r\n"
-        "\r\n"
-        "<!DOCTYPE html>\r\n"
-        "<html>\r\n"
-        "<head>\r\n"
-        "<title>Welcome to F-Stack!</title>\r\n"
-        "<style>\r\n"
-        "    body {  \r\n"
-        "        width: 35em;\r\n"
-        "        margin: 0 auto; \r\n"
-        "        font-family: Tahoma, Verdana, Arial, sans-serif;\r\n"
-        "    }\r\n"
-        "</style>\r\n"
-        "</head>\r\n"
-        "<body>\r\n"
-        "<h1>Welcome to F-Stack!</h1>\r\n"
-        "\r\n"
-        "<p>For online documentation and support please refer to\r\n"
-        "<a href=\"http://F-Stack.org/\">F-Stack.org</a>.<br/>\r\n"
-        "\r\n"
-        "<p><em>Thank you for using F-Stack.</em></p>\r\n"
-        "</body>\r\n"
-        "</html>";
+        return html;
     });
     function_handler* h2 = new function_handler([](std::unique_ptr<request> req) {
         return make_ready_future<json::json_return_type>("json-future");
