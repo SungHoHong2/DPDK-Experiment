b33c3de2b30d5298f327293b456487b2e5489249
SungHoHong2 <maverickjin88@gmail.com>
2018-01-12 12:44:16 -0700

    updated for DPDK-Experiments

--- seastar-experiment/seastar-server/server.cc
+++ seastar-experiment/seastar-server/server.cc
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
 
 
 seastar::future<> service_loop() {
-    socket_address sa;
+    seastar::socket_address sa;
     sa = seastar::make_ipv4_address({1234});
     std::cout << "server started at : " << sa << std::endl;
 
