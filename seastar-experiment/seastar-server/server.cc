c6d73e2f4adb6f7599a696287cbe0804f9907e5a
SungHoHong2 <maverickjin88@gmail.com>
2018-01-15 16:07:05 -0700

    updated for DPDK-Experiments

--- seastar-experiment/seastar-server/server.cc
+++ seastar-experiment/seastar-server/server.cc
@@ -138,8 +138,8 @@ void set_routes(routes& r) {
 
 int main(int ac, char** av) {
     app_template app;
-    // app.add_options()("port", bpo::value<uint16_t>()->default_value(10000),
-    //         "HTTP Server port");
+    app.add_options()("port", bpo::value<uint16_t>()->default_value(10000),
+            "HTTP Server port");
     // return app.run_deprecated(ac, av, [&] {
     //     auto&& config = app.configuration();
     //     uint16_t port = config["port"].as<uint16_t>();
