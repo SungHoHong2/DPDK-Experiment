3549496a8fff3908857e927f4e22275a692a0e50
SungHoHong2 <maverickjin88@gmail.com>
2018-01-15 16:09:53 -0700

    updated for DPDK-Experiments

--- seastar-experiment/seastar-server/server.cc
+++ seastar-experiment/seastar-server/server.cc
@@ -143,7 +143,7 @@ int main(int ac, char** av) {
     return app.run_deprecated(ac, av, [&] {
         auto&& config = app.configuration();
         // uint16_t port = config["port"].as<uint16_t>();
-        onfig["port"].as<uint16_t>();
+        config["port"].as<uint16_t>();
         // auto server = new http_server_control();
         // auto rb = make_shared<api_registry_builder>("apps/httpd/");
         // server->start().then([server] {
