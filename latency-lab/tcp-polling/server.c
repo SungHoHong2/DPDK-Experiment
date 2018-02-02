f115f538cd7894d2b144e15f9f332bbe63fa8545
SungHoHong2 <maverickjin88@gmail.com>
2018-02-02 15:57:04 -0700

    updated for DPDK-Experiments

--- latency-lab/tcp-polling/server.c
+++ latency-lab/tcp-polling/server.c
@@ -1,32 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
+#include <stdio.h>
+#define NUM_THREADS     5
 
-
-int g = 0;
-
-void *myThreadFun(void *vargp)
+void *PrintHello(void *threadid)
 {
-    // Store the value argument passed to this thread
-    int *myid = (int *)vargp;
-
-    // Let us create a static variable to observe its changes
-    static int s = 0;
-    // Change static and global variables
-    ++s; ++g;
-    // Print the argument, static and global variables
-    printf("Thread ID: %d, Static: %d, Global: %d\n", *myid, ++s, ++g);
+   long tid;
+   tid = (long)threadid;
+   printf("Hello World! It's me, thread #%ld!\n", tid);
+   pthread_exit(NULL);
 }
 
-
-int main()
+int main (int argc, char *argv[])
 {
-    int i;
-    pthread_t tid;
-    // Let us create three threads
-    for (i = 0; i < 3; i++)
-        pthread_create(&tid, NULL, myThreadFun, (void *)i);
-
-    pthread_exit(NULL);
-    return 0;
+   pthread_t threads[NUM_THREADS];
+   int rc;
+   long t;
+   for(t=0; t<NUM_THREADS; t++){
+      printf("In main: creating thread %ld\n", t);
+      rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+      if (rc){
+         printf("ERROR; return code from pthread_create() is %d\n", rc);
+         exit(-1);
+      }
+   }
+   pthread_exit(NULL);
 }
