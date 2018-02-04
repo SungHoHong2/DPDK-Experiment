48054d5705d34736351b767b5e5e5a130e0d7d17
SungHoHong2 <maverickjin88@gmail.com>
2018-02-04 16:09:49 -0700

    updated for DPDK-Experiments

---
+++ latency-experiment/tcp-polling/persocket.c
@@ -0,0 +1,146 @@
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<stdlib.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+#include<pthread.h> //for threading , link with lpthread
+
+char html[] =
+"HTTP/1.1 200 OK\r\n"
+"Server: F-Stack\r\n"
+"Date: Sat, 25 Feb 2017 09:26:33 GMT\r\n"
+"Content-Type: text/html\r\n"
+"Content-Length: 439\r\n"
+"Last-Modified: Tue, 21 Feb 2017 09:44:03 GMT\r\n"
+"Connection: keep-alive\r\n"
+"Accept-Ranges: bytes\r\n"
+"\r\n"
+"<!DOCTYPE html>\r\n"
+"<html>\r\n"
+"<head>\r\n"
+"<title>Welcome to F-Stack!</title>\r\n"
+"<style>\r\n"
+"    body {  \r\n"
+"        width: 35em;\r\n"
+"        margin: 0 auto; \r\n"
+"        font-family: Tahoma, Verdana, Arial, sans-serif;\r\n"
+"    }\r\n"
+"</style>\r\n"
+"</head>\r\n"
+"<body>\r\n"
+"<h1>Welcome to F-Stack!</h1>\r\n"
+"\r\n"
+"<p>For online documentation and support please refer to\r\n"
+"<a href=\"http://F-Stack.org/\">F-Stack.org</a>.<br/>\r\n"
+"\r\n"
+"<p><em>Thank you for using F-Stack.</em></p>\r\n"
+"</body>\r\n"
+"</html>";
+
+
+//the thread function
+void *connection_handler(void *);
+
+int main(int argc , char *argv[])
+{
+    int socket_desc , client_sock , c , *new_sock;
+    struct sockaddr_in server , client;
+
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( 8888 );
+
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+
+    //Listen
+    listen(socket_desc , 3);
+
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+
+
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+    while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+    {
+        puts("Connection accepted");
+
+        pthread_t sniffer_thread;
+        new_sock = malloc(1);
+        *new_sock = client_sock;
+
+        if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) new_sock) < 0)
+        {
+            perror("could not create thread");
+            return 1;
+        }
+
+        //Now join the thread , so that we dont terminate before the thread
+        //pthread_join( sniffer_thread , NULL);
+        puts("Handler assigned");
+    }
+
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+
+    return 0;
+}
+
+/*
+ * This will handle connection for each client
+ * */
+void *connection_handler(void *socket_desc)
+{
+    //Get the socket descriptor
+    int sock = *(int*)socket_desc;
+    int read_size;
+    char *message , client_message[250];
+
+    //Send some messages to the client
+    // message = "Greetings! I am your connection handler\n";
+    // write(sock , message , strlen(message));
+
+    // message = "Now type something and i shall repeat what you type \n";
+    // write(sock , message , strlen(message));
+
+    //Receive a message from client
+    while( (read_size = recv(sock , client_message , 250 , 0)) > 0 )
+    {
+        //Send the message back to client
+        // write(sock , client_message , strlen(client_message));
+        write(sock, html, sizeof(html));
+    }
+
+    if(read_size == 0)
+    {
+        // puts("Client disconnected");
+        fflush(stdout);
+    }
+
+    //Free the socket pointer
+    free(socket_desc);
+
+    return 0;
+}
