40432e75b674541d87f699828e0b36bb420c0678
SungHoHong2 <maverickjin88@gmail.com>
2018-02-04 11:19:35 -0700

    updated for DPDK-Experiments

--- latency-lab/tcp-polling/server.c
+++ latency-lab/tcp-polling/server.c
@@ -13,10 +13,12 @@ void *PrintHello(void *threadid)
 
 int main (int argc, char *argv[])
 {
-
-/home/users/sungho/MLNX_OFED_SRC-4.2-1.2.0.0/
   // add argument smp with number
   // allocate the pthread by pointers
+
+  int total_thread = argv[0];
+
+   printf("total num of thread: %d\n", total_thread);
 
    pthread_t threads[NUM_THREADS];
    int rc;
@@ -33,73 +35,3 @@ int main (int argc, char *argv[])
 
    return 0;
 }
-
-
-
-./configure --prefix=/home/users/sungho/libvma/usr 4 --includedir=/home/users/sungho/libvma/usr/include/mellanox --sysconfdir=/home/users/sungho/libvma/etc
-
-
-============================================================================
-== Configure project
-============================================================================
-checking for gcov support... no
-checking for time measurement support... no
-checking for valgrind support... no
-checking for OFED path... /home/users/sungho/libvma/usr
-checking infiniband/verbs.h usability... no
-checking infiniband/verbs.h presence... yes
-configure: WARNING: infiniband/verbs.h: present but cannot be compiled
-configure: WARNING: infiniband/verbs.h:     check for missing prerequisite headers?
-configure: WARNING: infiniband/verbs.h: see the Autoconf documentation
-configure: WARNING: infiniband/verbs.h:     section "Present But Cannot Be Compiled"
-configure: WARNING: infiniband/verbs.h: proceeding with the compiler's result
-configure: WARNING:     ## ----------------------------------- ##
-configure: WARNING:     ## Report this to support@mellanox.com ##
-configure: WARNING:     ## ----------------------------------- ##
-checking for infiniband/verbs.h... no
-configure: error: Unable to find the libibverbs-devel header files
-sungho@c1n14:~/libvma$ ./configure
-checking for a BSD-compatible install... /usr/bin/install -c
-checking whether build environment is sane... yes
-checking for a thread-safe mkdir -p... /bin/mkdir -p
-checking for gawk... gawk
-checking whether make sets $(MAKE)... yes
-checking whether make supports nested variables... yes
-checking whether make supports nested variables... (cached) yes
-============================================================================
-
-
-============================================================================
-== Configure project
-============================================================================
-checking for gcov support... no
-checking for time measurement support... no
-checking for valgrind support... no
-checking for OFED path... /usr
-checking infiniband/verbs.h usability... yes
-checking infiniband/verbs.h presence... yes
-checking for infiniband/verbs.h... yes
-checking rdma/rdma_cma.h usability... yes
-checking rdma/rdma_cma.h presence... yes
-checking for rdma/rdma_cma.h... yes
-checking for ibv_get_device_list in -libverbs... yes
-checking for rdma_create_id in -lrdmacm... yes
-checking for Mellanox OFED verbs (2.1 and older)... no
-checking for infiniband/mlx5_hw.h... yes
-checking for struct mlx5_qp.ctrl_seg... yes
-checking for struct mlx5_qp.gen_data... yes
-checking for attribute IBV_EXP_CQ_TIMESTAMP... yes
-checking for attribute IBV_EXP_VALUES_CLOCK_INFO... yes
-checking for attribute IBV_EXP_DEVICE_RX_CSUM_L4_PKT... no
-checking for attribute IBV_EXP_DEVICE_RX_CSUM_TCP_UDP_PKT... yes
-checking for attribute IBV_EXP_FLOW_SPEC_ACTION_TAG... yes
-checking for attribute IBV_QPT_RAW_PACKET... yes
-checking for attribute IBV_WC_WITH_VLAN... no
-checking for attribute IBV_ACCESS_ALLOCATE_MR... no
-checking for attribute IBV_FLOW_SPEC_IB... no
-checking for attribute IBV_DEVICE_RAW_IP_CSUM... no
-checking for attribute IBV_SEND_IP_CSUM... no
-checking for attribute IBV_EXP_CQ_MODERATION... yes
-checking for attribute IBV_EXP_WR_NOP... yes
-checking for attribute IBV_EXP_ACCESS_ALLOCATE_MR... yes
-checking for attribute IBV_EXP_QP_INIT_ATTR_ASSOCIATED_QPN... yes
