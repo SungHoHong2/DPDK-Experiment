57388ee6a415596e6c661a11aefc1cc0ac29a72e
SungHoHong2 <maverickjin88@gmail.com>
2018-02-04 11:39:14 -0700

    updated for DPDK-Experiments

--- latency-lab/tcp-polling/server.c
+++ latency-lab/tcp-polling/server.c
@@ -28,7 +28,7 @@ int main (int argc, char *argv[])
    long t;
    for(t=0; t<total_thread; t++){
       printf("In main: creating thread %ld\n", t);
-      rc = pthread_create(threads[t], NULL, PrintHello, (void *)t);
+      rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
     if (rc){
          printf("ERROR; return code from pthread_create() is %d\n", rc);
          exit(-1);
