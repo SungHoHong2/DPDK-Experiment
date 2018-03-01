1cd00483f42d5166c52e1aac4ebba33c0b75bf65
SungHoHong2 <maverickjin88@gmail.com>
2018-03-01 15:40:54 -0700

    updated for DPDK-Experiments

--- pingpong-experiment/client/l2fwd_launch_one_lcore.h
+++ pingpong-experiment/client/l2fwd_launch_one_lcore.h
@@ -152,8 +152,8 @@ static void l2fwd_main_loop(void){
 
 						rm[0] = rte_pktmbuf_alloc(test_pktmbuf_pool);
 						data = rte_pktmbuf_append(rm[0], PKT_SIZE);
-						//memset(data, '*', rte_pktmbuf_pkt_len(rm[0]));
-						rte_memcpy(data, "chara", 10);
+						rte_memcpy(data, '*', rte_pktmbuf_pkt_len(rm[0]));
+						// rte_memcpy(data, "chara", 10);
 						// printf("sending: %s\n",data);
 						rte_prefetch0(rte_pktmbuf_mtod(rm[0], void *));
 						l2fwd_mac_updating(rm[0], portid);
