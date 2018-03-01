699119d3fa4fd72771c10c00e16b7d3bd6555825
SungHoHong2 <maverickjin88@gmail.com>
2018-03-01 14:26:29 -0700

    updated for DPDK-Experiments

--- pingpong-experiment/server/l2fwd_launch_one_lcore.h
+++ pingpong-experiment/server/l2fwd_launch_one_lcore.h
@@ -1,27 +1,4 @@
 #include <time.h>
-#include <rte_common.h>
-#include <rte_log.h>
-#include <rte_malloc.h>
-#include <rte_memory.h>
-#include <rte_memcpy.h>
-#include <rte_memzone.h>
-#include <rte_eal.h>
-#include <rte_per_lcore.h>
-#include <rte_launch.h>
-#include <rte_atomic.h>
-#include <rte_cycles.h>
-#include <rte_prefetch.h>
-#include <rte_lcore.h>
-#include <rte_per_lcore.h>
-#include <rte_branch_prediction.h>
-#include <rte_interrupts.h>
-#include <rte_pci.h>
-#include <rte_random.h>
-#include <rte_debug.h>
-#include <rte_ether.h>
-#include <rte_ethdev.h>
-#include <rte_mempool.h>
-#include <rte_mbuf.h>
 
 
 /* Print out statistics on packets dropped */
@@ -161,10 +138,9 @@ static void l2fwd_main_loop(void){
 						for (j = 0; j < nb_rx; j++) {
 								m = pkts_burst[j];
 
-								char *data;
 								char *rtn;
-								rtn = rte_pktmbuf_mtod_offset(m, char *, sizeof(data);
-								printf("received: %s\n", rtn);
+								rtn = rte_pktmbuf_mtod_offset(m, char *, 80);
+								// printf("received: %s\n", rtn);
 
 								// port_statistics[portid].rx += rte_pktmbuf_pkt_len(m);
 								rte_prefetch0(rte_pktmbuf_mtod(m, void *));
