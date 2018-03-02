acc36db1c5fecc41c48962218ea78947ee9e3c8a
SungHoHong2 <maverickjin88@gmail.com>
2018-03-01 18:51:30 -0700

    updated for DPDK-Experiments

--- pingpong-experiment/client/l2fwd_launch_one_lcore.h
+++ pingpong-experiment/client/l2fwd_launch_one_lcore.h
@@ -59,6 +59,7 @@ l2fwd_mac_updating(struct rte_mbuf *m, unsigned dest_portid){
 	tmp = &eth->d_addr.addr_bytes[0];
 
 	// A0:36:9F:83:AB:BD
+	*((uint64_t *)tmp) = 0x0000000002 + ((uint64_t)dest_portid << 40);
 	// *((uint64_t *)tmp) = 0xbdab839f36a0 + ((uint64_t)dest_portid << 40);
 
 	/* src addr */
