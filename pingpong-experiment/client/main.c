06291c1b727353306e7536f065f9e0e8cef23512
SungHoHong2 <maverickjin88@gmail.com>
2018-03-01 20:46:44 -0700

    updated for DPDK-Experiments

--- pingpong-experiment/client/main.c
+++ pingpong-experiment/client/main.c
@@ -45,9 +45,9 @@
 //#define MAX_PKT_BURST 32
 #define MAX_PKT_BURST 32
 #define RTE_MBUF_SIZE 2048 // RTE_MBUF_DEFAULT_BUF_SIZE
-static int PKT_SIZE = 64; // sending packet size over 1600 the performance degrades
+static const int PKT_SIZE = 64; // sending packet size over 1600 the performance degrades
 static int LATENCY = 1;
-static int PINGS = 100000;
+static const int PINGS = 100000;
 static int LIMIT = PINGS*PKT_SIZE;
 static uint64_t start_time, end_time;
 
