4856d35a10301eb9b6d25ce667bc6521ce157b06
SungHoHong2 <maverickjin88@gmail.com>
2018-03-01 17:10:18 -0700

    updated for DPDK-Experiments

--- seastar-experiment/seastar-pingpong/client.cc
+++ seastar-experiment/seastar-pingpong/client.cc
@@ -8,12 +8,12 @@ using namespace net;
 using namespace std::chrono_literals;
 
 static time_t start; //adding timer
-const size_t BUFFER_SIZE = 10;
+static size_t BUFFER_SIZE = 10;
 static std::string packetz(BUFFER_SIZE,'*');
-const int LATENCY = 0, LIMIT = 100000;
-const int THROUGHPUT = 1, TIMER = 1;
-int total_throughput = 0;
-uint64_t start_time, end_time;
+static int LATENCY = 0, LIMIT = 100000;
+static int THROUGHPUT = 1, TIMER = 1;
+static int total_throughput = 0;
+static uint64_t start_time, end_time;
 
 uint64_t getTimeStamp() {
     struct timeval tv;
