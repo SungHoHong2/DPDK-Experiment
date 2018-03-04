0f0b457a74b82ef58276daf8e0baca5ffffe4576
SungHoHong2 <maverickjin88@gmail.com>
2018-03-03 21:12:09 -0700

    updated for DPDK-Experiments

--- pingpong-experiment/client/l2fwd_launch_one_lcore.h
+++ pingpong-experiment/client/l2fwd_launch_one_lcore.h
@@ -166,15 +166,15 @@ static void l2fwd_main_loop(void){
       else {
           // sending by bulk
           unsigned i;
-          struct rte_mbuf *mrm[PKT_NUMBUF];
+          struct rte_mbuf *mrm[NB_MBUF];
           int ret = 0;
 
-          for (i=0; i<PKT_NUMBUF; i++)
+          for (i=0; i<NB_MBUF; i++)
           	mrm[i] = NULL;
 
           char *data;
           /* alloc NB_MBUF mbufs */
-          for (i=0; i<PKT_NUMBUF; i++) {
+          for (i=0; i<NB_MBUF; i++) {
           	mrm[i] = rte_pktmbuf_alloc(test_pktmbuf_pool);
           	data = rte_pktmbuf_append(mrm[i], PKT_SIZE);
           	// memset(data, 0xff, rte_pktmbuf_pkt_len(mrm[i]));
@@ -194,7 +194,7 @@ static void l2fwd_main_loop(void){
           	port_statistics[portid].tx += sent; //* rte_pktmbuf_pkt_len(rm[0]);
           }
 
-          for (i=0; i<PKT_NUMBUF; i++)
+          for (i=0; i<NB_MBUF; i++)
           		rte_pktmbuf_free(mrm[i]);
 
       }
