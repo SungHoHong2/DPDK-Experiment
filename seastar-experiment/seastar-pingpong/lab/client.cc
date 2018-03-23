a314b3f7248c750576f256c35cd67debd2cab062
SungHoHong2 <maverickjin88@gmail.com>
2018-03-23 13:31:39 -0700

    updated for DPDK-Experiments

--- seastar-experiment/seastar-pingpong/lab/client.cc
+++ seastar-experiment/seastar-pingpong/lab/client.cc
@@ -1,33 +1,25 @@
 #include "core/app-template.hh"
 #include "core/future-util.hh"
 #include "core/distributed.hh"
-#include <sys/time.h>
 
 using namespace seastar;
 using namespace net;
 using namespace std::chrono_literals;
 
-time_t start; //adding timer
-size_t BUFFER_SIZE = 64;
-int LATENCY = 1, PINGS = 1000000;
-double total_throughput = 0;
-uint64_t start_time, end_time;
-std::string packetz(BUFFER_SIZE,'*');
-
-
-uint64_t getTimeStamp() {
-    struct timeval tv;
-    gettimeofday(&tv,NULL);
-    return tv.tv_sec*(uint64_t)1000000+tv.tv_usec;
-}
+static int rx_msg_size = 4 * 1024;
+static int tx_msg_total_size = 100 * 1024 * 1024;
+static int tx_msg_size = 4 * 1024;
+static int tx_msg_nr = tx_msg_total_size / tx_msg_size;
+static std::string str_txbuf(tx_msg_size, 'X');
 
 class client;
 distributed<client> clients;
+
 transport protocol = transport::TCP;
 
 class client {
 private:
-    static constexpr unsigned _pings_per_connection = 10;
+    static constexpr unsigned _pings_per_connection = 10000;
     unsigned _total_pings;
     unsigned _concurrent_connections;
     ipv4_addr _server_addr;
@@ -49,49 +41,143 @@ public:
             , _read_buf(_fd.input())
             , _write_buf(_fd.output()) {}
 
+        future<> do_read() {
+            return _read_buf.read_exactly(rx_msg_size).then([this] (temporary_buffer<char> buf) {
+                _bytes_read += buf.size();
+                if (buf.size() == 0) {
+                    return make_ready_future();
+                } else {
+                    return do_read();
+                }
+            });
+        }
+
+        future<> do_write(int end) {
+            if (end == 0) {
+                return make_ready_future();
+            }
+            return _write_buf.write(str_txbuf).then([this] {
+                _bytes_write += tx_msg_size;
+                return _write_buf.flush();
+            }).then([this, end] {
+                return do_write(end - 1);
+            });
+        }
+
         future<> ping(int times) {
-            std::string packeti(BUFFER_SIZE,'*');
-            return _write_buf.write(packeti).then([this] {
+            return _write_buf.write("ping").then([this] {
                 return _write_buf.flush();
             }).then([this, times] {
-                return _read_buf.read_exactly(BUFFER_SIZE).then([this, times] (temporary_buffer<char> buf) {
-                    auto str = std::string(buf.get(), buf.size());
-                    // if (str != packetz) {
-                    //     std::cout << str << str.length() << std::endl;
-                    //     return make_ready_future();
-                    // }
-                    total_throughput+=str.length();
-
-                    if(LATENCY && total_throughput >= (BUFFER_SIZE*PINGS)){
+                return _read_buf.read_exactly(4).then([this, times] (temporary_buffer<char> buf) {
+                    if (buf.size() != 4) {
+                        fprint(std::cerr, "illegal packet received: %d\n", buf.size());
                         return make_ready_future();
                     }
-                    // std::cout << str << str.length() << std::endl;
-                    return ping(times);
+                    auto str = std::string(buf.get(), buf.size());
+                    if (str != "pong") {
+                        fprint(std::cerr, "illegal packet received: %d\n", buf.size());
+                        return make_ready_future();
+                    }
+                    if (times > 0) {
+                        return ping(times - 1);
+                    } else {
+                        return make_ready_future();
+                    }
+                });
+            });
+        }
+
+        future<size_t> rxrx() {
+            return _write_buf.write("rxrx").then([this] {
+                return _write_buf.flush();
+            }).then([this] {
+                return do_write(tx_msg_nr).then([this] {
+                    return _write_buf.close();
+                }).then([this] {
+                    return make_ready_future<size_t>(_bytes_write);
+                });
+            });
+        }
+
+        future<size_t> txtx() {
+            return _write_buf.write("txtx").then([this] {
+                return _write_buf.flush();
+            }).then([this] {
+                return do_read().then([this] {
+                    return make_ready_future<size_t>(_bytes_read);
                 });
             });
         }
     };
 
     future<> ping_test(connection *conn) {
-
-        return conn->ping(_pings_per_connection).then([] {
-            clients.invoke_on(0, &client::ping_report);
+        auto started = lowres_clock::now();
+        return conn->ping(_pings_per_connection).then([started] {
+            auto finished = lowres_clock::now();
+            clients.invoke_on(0, &client::ping_report, started, finished);
         });
     }
 
+    future<> rxrx_test(connection *conn) {
+        auto started = lowres_clock::now();
+        return conn->rxrx().then([started] (size_t bytes) {
+            auto finished = lowres_clock::now();
+            clients.invoke_on(0, &client::rxtx_report, started, finished, bytes);
+        });
+    }
 
-    void ping_report() {
+    future<> txtx_test(connection *conn) {
+        auto started = lowres_clock::now();
+        return conn->txtx().then([started] (size_t bytes) {
+            auto finished = lowres_clock::now();
+            clients.invoke_on(0, &client::rxtx_report, started, finished, bytes);
+        });
+    }
+
+    void ping_report(lowres_clock::time_point started, lowres_clock::time_point finished) {
+        if (_earliest_started > started)
+            _earliest_started = started;
+        if (_latest_finished < finished)
+            _latest_finished = finished;
         if (++_num_reported == _concurrent_connections) {
-            end_time = getTimeStamp();
-            printf("sending the %d pings using %ld byte packet\n", PINGS, BUFFER_SIZE);
-            printf("latency: %ld\n", end_time - start_time);
-            std::cout << "throughput: " <<  (total_throughput/1048576)/((end_time - start_time)/1000000) << " Mbytes" << std::endl;
+            auto elapsed = _latest_finished - _earliest_started;
+            auto usecs = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+            auto secs = static_cast<double>(usecs) / static_cast<double>(1000 * 1000);
+            fprint(std::cout, "========== ping ============\n");
+            fprint(std::cout, "Server: %s\n", _server_addr);
+            fprint(std::cout,"Connections: %u\n", _concurrent_connections);
+            fprint(std::cout, "Total PingPong: %u\n", _total_pings);
+            fprint(std::cout, "Total Time(Secs): %f\n", secs);
+            fprint(std::cout, "Requests/Sec: %f\n",
+                static_cast<double>(_total_pings) / secs);
             clients.stop().then([] {
                 engine().exit(0);
             });
         }
     }
 
+    void rxtx_report(lowres_clock::time_point started, lowres_clock::time_point finished, size_t bytes) {
+        if (_earliest_started > started)
+            _earliest_started = started;
+        if (_latest_finished < finished)
+            _latest_finished = finished;
+        _processed_bytes += bytes;
+        if (++_num_reported == _concurrent_connections) {
+            auto elapsed = _latest_finished - _earliest_started;
+            auto usecs = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+            auto secs = static_cast<double>(usecs) / static_cast<double>(1000 * 1000);
+            fprint(std::cout, "========== %s ============\n", _test);
+            fprint(std::cout, "Server: %s\n", _server_addr);
+            fprint(std::cout, "Connections: %u\n", _concurrent_connections);
+            fprint(std::cout, "Bytes Received(MiB): %u\n", _processed_bytes/1024/1024);
+            fprint(std::cout, "Total Time(Secs): %f\n", secs);
+            fprint(std::cout, "Bandwidth(Gbits/Sec): %f\n",
+                static_cast<double>((_processed_bytes * 8)) / (1000 * 1000 * 1000) / secs);
+            clients.stop().then([] {
+                engine().exit(0);
+            });
+        }
+    }
 
     future<> start(ipv4_addr server_addr, std::string test, unsigned ncon) {
         _server_addr = server_addr;
@@ -128,18 +214,11 @@ namespace bpo = boost::program_options;
 int main(int ac, char ** av) {
     app_template app;
     app.add_options()
-        ("server", bpo::value<std::string>()->default_value("10.218.111.252:1234"), "Server address")
+        ("server", bpo::value<std::string>()->required(), "Server address")
         ("test", bpo::value<std::string>()->default_value("ping"), "test type(ping | rxrx | txtx)")
-        ("conn", bpo::value<unsigned>()->default_value(2), "nr connections per cpu")
+        ("conn", bpo::value<unsigned>()->default_value(16), "nr connections per cpu")
         ("proto", bpo::value<std::string>()->default_value("tcp"), "transport protocol tcp|sctp")
-        ("buffer", bpo::value<unsigned>()->default_value(64), "buffer size")
         ;
-
-    // INITIALIZE THE TEST BEGIN
-    if (LATENCY){
-               start_time = getTimeStamp();
-               total_throughput = 0;
-    }
 
     return app.run_deprecated(ac, av, [&app] {
         auto&& config = app.configuration();
@@ -147,7 +226,6 @@ int main(int ac, char ** av) {
         auto test = config["test"].as<std::string>();
         auto ncon = config["conn"].as<unsigned>();
         auto proto = config["proto"].as<std::string>();
-        BUFFER_SIZE = config["buffer"].as<unsigned>();
 
         if (proto == "tcp") {
             protocol = transport::TCP;
@@ -158,13 +236,19 @@ int main(int ac, char ** av) {
             return engine().exit(1);
         }
 
+        if (!client::tests.count(test)) {
+            fprint(std::cerr, "Error: -test=ping | rxrx | txtx\n");
+            return engine().exit(1);
+        }
+
         clients.start().then([server, test, ncon] () {
             clients.invoke_on_all(&client::start, ipv4_addr{server}, test, ncon);
         });
-
     });
 }
 
 const std::map<std::string, client::test_fn> client::tests = {
         {"ping", &client::ping_test},
+        {"rxrx", &client::rxrx_test},
+        {"txtx", &client::txtx_test},
 };
--- seastar-experiment/seastar-pingpong/lab/server.cc
+++ seastar-experiment/seastar-pingpong/lab/server.cc
@@ -6,21 +6,44 @@
 #include <iostream>
 
 using namespace seastar;
-const size_t BUFFER_SIZE = 10;
+
+static std::string str_ping{"ping"};
+static std::string str_txtx{"txtx"};
+static std::string str_rxrx{"rxrx"};
+static std::string str_pong{"pong"};
+static std::string str_unknow{"unknow cmd"};
+static int tx_msg_total_size = 100 * 1024 * 1024;
+static int tx_msg_size = 4 * 1024;
+static int tx_msg_nr = tx_msg_total_size / tx_msg_size;
+static int rx_msg_size = 4 * 1024;
+static std::string str_txbuf(tx_msg_size, 'X');
+static bool enable_tcp = false;
+static bool enable_sctp = false;
 
 class tcp_server {
     std::vector<server_socket> _tcp_listeners;
-
+    std::vector<server_socket> _sctp_listeners;
 public:
     future<> listen(ipv4_addr addr) {
-        listen_options lo;
-        lo.proto = transport::TCP;
-        lo.reuse_address = true;
-        _tcp_listeners.push_back(engine().listen(make_ipv4_address(addr), lo));
-        do_accepts(_tcp_listeners);
+        if (enable_tcp) {
+            listen_options lo;
+            lo.proto = transport::TCP;
+            lo.reuse_address = true;
+            _tcp_listeners.push_back(engine().listen(make_ipv4_address(addr), lo));
+            do_accepts(_tcp_listeners);
+        }
+
+        if (enable_sctp) {
+            listen_options lo;
+            lo.proto = transport::SCTP;
+            lo.reuse_address = true;
+            _sctp_listeners.push_back(engine().listen(make_ipv4_address(addr), lo));
+            do_accepts(_sctp_listeners);
+        }
         return make_ready_future<>();
     }
 
+    // FIXME: We should properly tear down the service here.
     future<> stop() {
         return make_ready_future<>();
     }
@@ -63,17 +86,34 @@ public:
                 return make_ready_future();
             }
             // Expect 4 bytes cmd from client
-            return _read_buf.read_exactly(BUFFER_SIZE).then([this] (temporary_buffer<char> buf) {
+            size_t n = 4;
+            return _read_buf.read_exactly(n).then([this] (temporary_buffer<char> buf) {
                 if (buf.size() == 0) {
                     return make_ready_future();
                 }
                 auto cmd = std::string(buf.get(), buf.size());
                 // pingpong test
-                    return _write_buf.write(cmd).then([this] {
+                if (cmd == str_ping) {
+                    // this is where you get the string ping!
+                    return _write_buf.write(str_pong).then([this] {
                         return _write_buf.flush();
                     }).then([this] {
                         return this->read();
                     });
+                // server tx test
+                } else if (cmd == str_txtx) {
+                    return tx_test();
+                // server tx test
+                } else if (cmd == str_rxrx) {
+                    return rx_test();
+                // unknow test
+                } else {
+                    return _write_buf.write(str_unknow).then([this] {
+                        return _write_buf.flush();
+                    }).then([] {
+                        return make_ready_future();
+                    });
+                }
             });
         }
         future<> do_write(int end) {
@@ -86,13 +126,25 @@ public:
                 return do_write(end - 1);
             });
         }
+        future<> tx_test() {
+            return do_write(tx_msg_nr).then([this] {
+                return _write_buf.close();
+            }).then([] {
+                return make_ready_future<>();
+            });
+        }
         future<> do_read() {
             return _read_buf.read_exactly(rx_msg_size).then([this] (temporary_buffer<char> buf) {
                 if (buf.size() == 0) {
                     return make_ready_future();
                 } else {
-                    return do_read();  // read the file until it
+                    return do_read();
                 }
+            });
+        }
+        future<> rx_test() {
+            return do_read().then([] {
+                return make_ready_future<>();
             });
         }
     };
@@ -102,13 +154,28 @@ namespace bpo = boost::program_options;
 
 int main(int ac, char** av) {
     app_template app;
+    app.add_options()
+        ("port", bpo::value<uint16_t>()->default_value(1234), "TCP server port") // add port
+        ("tcp", bpo::value<std::string>()->default_value("yes"), "tcp listen") // add tcp
+        ("sctp", bpo::value<std::string>()->default_value("no"), "sctp listen"); // enable sctp
+
 
     return app.run_deprecated(ac, av, [&] {
-        uint16_t port = 1234; // assign the port value from the app_template
+        auto&& config = app.configuration();
+        uint16_t port = config["port"].as<uint16_t>(); // assign the port value from the app_template
+        enable_tcp = config["tcp"].as<std::string>() == "yes"; // assign the tcp key from the app_template
+        enable_sctp = config["sctp"].as<std::string>() == "yes"; // assign the sctp key from the app_template
+        if (!enable_tcp && !enable_sctp) {
+            fprint(std::cerr, "Error: no protocols enabled. Use \"--tcp yes\" and/or \"--sctp yes\" to enable\n");
+            return engine().exit(1);
+        } // if there is no specific value then return false
+
+
         auto server = new distributed<tcp_server>; // run distributed object
         // The distributed template manages a sharded service,
         // by creating a copy of the service on each shard, providing mechanisms
         // to communicate with each shard's copy, and a way to stop the service.
+
 
         //Starts Service by constructing an instance on every logical core with a copy of args passed to the constructor.
         server->start().then([server = std::move(server), port] () mutable {
