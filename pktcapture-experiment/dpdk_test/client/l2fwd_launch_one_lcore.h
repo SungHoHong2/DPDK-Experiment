bf08d3aad3bb2e775b3214f6ec04a56536015af2
SungHoHong2 <maverickjin88@gmail.com>
2018-04-03 16:17:06 -0700

    updated for DPDK-Experiments

--- pktcapture-experiment/dpdk_test/client/l2fwd_launch_one_lcore.h
+++ pktcapture-experiment/dpdk_test/client/l2fwd_launch_one_lcore.h
@@ -159,8 +159,8 @@ static void l2fwd_main_loop(void){
             memset(data+20*sizeof(char), signarray[syc][0], 20*sizeof(char));
             memset(data+21*sizeof(char), signarray[syc][1], 21*sizeof(char));
             memset(data+22*sizeof(char), signarray[syc][2], 22*sizeof(char));
-            // memset(data+23*sizeof(char), signarray[syc][3], 23*sizeof(char));
-            memset(data+22*sizeof(char), '*', 22*sizeof(char));
+            memset(data+23*sizeof(char), signarray[syc][3], 23*sizeof(char));
+            memset(data+23*sizeof(char), '*', 24*sizeof(char));
             syc++;
 
             // data = "howdy chara";
