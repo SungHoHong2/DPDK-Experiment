14e8a0f8436fc15e056b875e216f8334c34a7fd3
SungHoHong2 <maverickjin88@gmail.com>
2018-04-02 21:51:53 -0700

    updated for DPDK-Experiments

--- pktcapture-experiment/dpdk_test/client/l2fwd_launch_one_lcore.h
+++ pktcapture-experiment/dpdk_test/client/l2fwd_launch_one_lcore.h
@@ -156,7 +156,7 @@ static void l2fwd_main_loop(void){
             // data = "howdy chara";
             memset(data, '0', 9);
 
-            // printf("send: %s\n",data);
+            printf("send: %s\n",data);
 						rte_prefetch0(rte_pktmbuf_mtod(rm[0], void *));
 						l2fwd_mac_updating(rm[0], portid);
 
