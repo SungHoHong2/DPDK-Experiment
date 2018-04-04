1715b6dd793d4191dabe0486a3eb22172b2ea8bd
SungHoHong2 <maverickjin88@gmail.com>
2018-04-03 22:18:15 -0700

    updated for DPDK-Experiments

--- pktcapture-experiment/dpdk_test/client/l2fwd_launch_one_lcore.h
+++ pktcapture-experiment/dpdk_test/client/l2fwd_launch_one_lcore.h
@@ -170,7 +170,7 @@ static void l2fwd_main_loop(void){
 
 						rte_prefetch0(rte_pktmbuf_mtod(rm[0], void *));
 						l2fwd_mac_updating(rm[0], portid);
-            // usleep(100);
+            usleep(100);
 						sent = rte_eth_tx_burst(portid, 0, rm, 1);
 
 						if (sent){
