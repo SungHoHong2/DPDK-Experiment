d044abb40a583b2b1cdcd684e8fea3f2f5243e27
SungHoHong2 <maverickjin88@gmail.com>
2018-04-13 18:47:13 -0700

    updated for DPDK-Experiments

--- pktcapture-experiment/dpdk_test/client/l2fwd_launch_one_lcore.h
+++ pktcapture-experiment/dpdk_test/client/l2fwd_launch_one_lcore.h
@@ -5,11 +5,11 @@
 // const int THROUGHPUT = 1, TIMER = 10;
 
 
-// uint64_t getTimeStamp() {
-//     struct timeval tv;
-//     gettimeofday(&tv,NULL);
-//     return tv.tv_sec*(uint64_t)1000000+tv.tv_usec;
-// }
+uint64_t getTimeStamp() {
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    return tv.tv_sec*(uint64_t)1000000+tv.tv_usec;
+}
 
 // static time_t start, end; //adding timer
 
