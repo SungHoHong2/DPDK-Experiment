b3832eb5ec687da4a0f307f07c5e80e51a801fed
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-10 12:58:07 -0700

    updated for DPDK-Experiments

--- dpdk-suite/dpdk_test/pkt-gen/transfer.sh
+++ dpdk-suite/dpdk_test/pkt-gen/transfer.sh
@@ -2,4 +2,4 @@ git pull origin master
 wait
 cp throughput.lua /home/sungho/pktgen-3.4.2/throughput.lua
 cd /home/sungho/pktgen-3.4.2
-/app/x86_64-native-linuxapp-gcc/pktgen -l 0-4 -n 3 -- -P -m "[2:2].1" -f throughput.lua
+app/x86_64-native-linuxapp-gcc/pktgen -l 0-4 -n 3 -- -P -m "[2:2].1" -f throughput.lua
