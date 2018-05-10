b96e1b19eef8df249a91c3df70179fc931af7cfb
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-10 11:29:17 -0700

    updated for DPDK-Experiments

--- dpdk-suite/dpdk_test/pkt-gen/throughput.lua
+++ dpdk-suite/dpdk_test/pkt-gen/throughput.lua
@@ -11,10 +11,14 @@ pktgen.dst_mac("1", "start", "00:1B:21:A6:D4:D5");
 pktgen.start("1");
 pktgen.delay(5000);
 
+-- pause
 pktgen.cls();
+pktgen.screen("off");
 pktgen.clear("all");
 pktgen.reset("all");
+-- pause
 
+pktgen.screen("throughput measurement test\n");
 pktgen.pause("test case 2\n", 2000);
 
 pktgen.set("all", "count", 100000);
