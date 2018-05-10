3886ebe0fa4156f4b69d9c6992ded8187a3dd48a
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-10 12:57:41 -0700

    updated for DPDK-Experiments

--- dpdk-suite/dpdk_test/pkt-gen/throughput.lua
+++ dpdk-suite/dpdk_test/pkt-gen/throughput.lua
@@ -19,7 +19,6 @@ pktgen.clear("all");
 pktgen.reset("all");
 -- pause
 
-pktgen.screen("throughput measurement test\n");
 pktgen.pause("test case 2\n", 2000);
 
 -- pktgen.set("all", "count", 100000);
@@ -30,3 +29,10 @@ pktgen.screen("on");
 pktgen.dst_mac("1", "start", "00:1B:21:A6:D4:D5");
 pktgen.start("1");
 pktgen.delay(10000);
+
+pktgen.cls();
+pktgen.screen("off");
+pktgen.clear("all");
+pktgen.reset("all");
+
+pktgen.pause("end"\n", 2000);
--- dpdk-suite/dpdk_test/pkt-gen/transfer.sh
+++ dpdk-suite/dpdk_test/pkt-gen/transfer.sh
@@ -1,3 +1,5 @@
 git pull origin master
 wait
 cp throughput.lua /home/sungho/pktgen-3.4.2/throughput.lua
+cd /home/sungho/pktgen-3.4.2
+/app/x86_64-native-linuxapp-gcc/pktgen -l 0-4 -n 3 -- -P -m "[2:2].1" -f throughput.lua
