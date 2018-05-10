7016e47781db678a841f8e0e2295c3c7809b8e82
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-10 11:26:53 -0700

    updated for DPDK-Experiments

--- dpdk-suite/dpdk_test/pkt-gen/throughput.lua
+++ dpdk-suite/dpdk_test/pkt-gen/throughput.lua
@@ -9,3 +9,19 @@ pktgen.screen("on");
 
 pktgen.dst_mac("1", "start", "00:1B:21:A6:D4:D5");
 pktgen.start("1");
+pktgen.delay(5000);
+
+pktgen.cls();
+pktgen.clear("all");
+pktgen.reset("all");
+
+pktgen.pause("test case 2\n", 2000);
+
+pktgen.set("all", "count", 100000);
+pktgen.set("all", "size", 256);
+pktgen.set("all", "burst", 128);
+pktgen.screen("on");
+
+pktgen.dst_mac("1", "start", "00:1B:21:A6:D4:D5");
+pktgen.start("1");
+pktgen.delay(5000);
