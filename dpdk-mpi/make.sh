80d872eae790063e32aca5ce6b30ccd21c275332
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-10 18:10:23 -0700

    updated for DPDK-Experiments

--- dpdk-mpi/make.sh
+++ dpdk-mpi/make.sh
@@ -6,5 +6,5 @@ if [ "$1" = "mpi_send_recv" ]
 then
 mpicc -o mpi-send-recv mpi-send-recv.c
 wait
-mpirun -n 1 -hostfile hosts_file ./mpi-send-recv
+mpirun -n 2 -hostfile hosts_file ./mpi-send-recv
 fi
