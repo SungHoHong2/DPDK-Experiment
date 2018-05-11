386175a0a4ec9a4a49901697141f79712bbe856a
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-10 18:12:33 -0700

    updated for DPDK-Experiments

--- dpdk-mpi/make.sh
+++ dpdk-mpi/make.sh
@@ -1,10 +1,11 @@
 export MPICC=/home/sungho/mpich
 export PATH=/home/you/mpich-install/bin:$PATH
 sudo git pull origin master
-
+wait
 if [ "$1" = "mpi_send_recv" ]
 then
 mpicc -o mpi-send-recv mpi-send-recv.c
 wait
-mpirun -n 2 -hostfile hosts_file ./mpi-send-recv
+# mpirun -n 2 -hostfile hosts_file ./mpi-send-recv
+mpirun -n 2 ./send_recv
 fi
