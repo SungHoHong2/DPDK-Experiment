c0f4cff5f064c4b874025981360c24370c7d95a9
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-10 18:37:40 -0700

    updated for DPDK-Experiments

--- dpdk-mpi/make.sh
+++ dpdk-mpi/make.sh
@@ -5,17 +5,13 @@ wait
 if [ "$1" = "hellow" ]
 then
 sudo mpicc -o hellow hellow.c
-# wait
-elif [ "$1" = "hellow_run" ]
-then
+wait
 mpirun -n 4 -hostfile hosts_file ./hellow
 
 elif [ "$1" = "mpi_send_recv" ]
 then
 sudo mpicc -o mpi-send-recv mpi-send-recv.c
-
-elif [ "$1" = "mpi_send_recv_run" ]
-then
+wait
 mpirun -n 2 -hostfile hosts_file ./mpi-send-recv
 
 fi
