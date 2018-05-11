53e5588c05cfbef7249286184c9275ad925f02bd
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-10 18:15:11 -0700

    updated for DPDK-Experiments

--- dpdk-mpi/make.sh
+++ dpdk-mpi/make.sh
@@ -2,7 +2,13 @@ export MPICC=/home/sungho/mpich
 export PATH=/home/you/mpich-install/bin:$PATH
 sudo git pull origin master
 wait
-if [ "$1" = "mpi_send_recv" ]
+if [ "$1" = "hellow" ]
+then
+mpicc -o hellow hellow.c
+wait
+mpirun -n 4 -hostfile hosts_file ./hellow
+
+elif [ "$1" = "mpi_send_recv" ]
 then
 mpicc -o mpi-send-recv mpi-send-recv.c
 wait
