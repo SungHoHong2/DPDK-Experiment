32ce18b79aa32ac607deaaddc3b63191274cf264
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-11 15:48:01 -0700

    updated for DPDK-Experiments

--- dpdk-mpi/communication.c
+++ dpdk-mpi/communication.c
@@ -7,7 +7,8 @@ int main( int argc, char *argv[] )
     int rank;
     int size;
     int numbertoreceive, numbertosend=77;
-
+    MPI_Status status;
+    
     MPI_Init( 0, 0 );
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
