83c1ce46e5a6b74c50c3cd04664649848a07165c
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-10 20:09:14 -0700

    updated for DPDK-Experiments

--- dpdk-mpi/mpi-send-recv.c
+++ dpdk-mpi/mpi-send-recv.c
@@ -1,35 +1,21 @@
-#include <mpi.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include < mpi.h>   /* PROVIDES THE BASIC MPI DEFINITION AND TYPES */
+#include  < stdio.h>
 
-int main(int argc, char** argv) {
-  // Initialize the MPI environment
-  MPI_Init(0, 0);
-  // Find out rank, size
-  int world_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-  int world_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+int main(int argc, char **argv) {
 
-  printf("%d\n", world_rank);
-  printf("%d\n", world_size);
+  int my_rank;
+  int size;
+  MPI_Init(&argc, &argv); /*START MPI */
+
+/*DETERMINE RANK OF THIS PROCESSOR*/
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+ /*DETERMINE TOTAL NUMBER OF PROCESSORS*/
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 
-  // // We are assuming at least 2 processes for this task
-  // if (world_size < 2) {
-  //   fprintf(stderr, "World size must be greater than 1 for %s\n", argv[0]);
-  //   MPI_Abort(MPI_COMM_WORLD, 1);
-  // }
-  //
-  int number;
-  if (world_rank == 0) {
-    // If we are rank 0, set the number to -1 and send it to process 1
-    number = -1;
-    MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-  }
-  // } else if (world_rank == 1) {
-  //   MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  //   printf("Process 1 received number %d from process 0\n", number);
-  // }
-  MPI_Finalize();
+  printf("Hello world! I'm rank (processor number) %d of size %d\n", my_rank, size);
+
+  MPI_Finalize();  /* EXIT MPI */
+
 }
