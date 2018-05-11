658aae23cc46960733aac750758f0132284f41e8
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-11 15:16:32 -0700

    updated for DPDK-Experiments

--- dpdk-mpi/make.sh
+++ dpdk-mpi/make.sh
@@ -4,10 +4,11 @@ sudo git pull origin master
 
 
 if [ "$1" = "" ]
-echo "I will just update\n"
 then
+echo "I will just update\n"
 
 else
+then 
 sudo mpicc -o $1 $1.c
 wait
 mpirun -n 2 -hostfile hosts_file ./$1
