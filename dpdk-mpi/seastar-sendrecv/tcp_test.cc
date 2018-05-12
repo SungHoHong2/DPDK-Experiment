b1b080e4461d12473413189f770c7ebf1143c959
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-12 10:06:11 -0700

    updated for DPDK-Experiments

--- dpdk-mpi/seastar-sendrecv/tcp_test.cc
+++ dpdk-mpi/seastar-sendrecv/tcp_test.cc
@@ -1,48 +1,67 @@
-#include "net/ip.hh"
-#include "net/virtio.hh"
-#include "net/tcp.hh"
+/*
+ * This file is open source software, licensed to you under the terms
+ * of the Apache License, Version 2.0 (the "License").  See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.  You may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * Copyright 2015 Cloudius Systems
+ */
+#include <cmath>
+#include "core/reactor.hh"
+#include "core/app-template.hh"
+#include "core/sleep.hh"
+#include "net/dns.hh"
+#include "tls_echo_server.hh"
 
 using namespace seastar;
-using namespace net;
+namespace bpo = boost::program_options;
 
-struct tcp_test {
-    ipv4& inet;
-    using tcp = net::tcp<ipv4_traits>;
-    tcp::listener _listener;
-    struct connection {
-        tcp::connection tcp_conn;
-        explicit connection(tcp::connection tc) : tcp_conn(std::move(tc)) {}
-        void run() {
-            tcp_conn.wait_for_data().then([this] {
-                auto p = tcp_conn.read();
-                if (!p.len()) {
-                    tcp_conn.close_write();
-                    return;
-                }
-                print("read %d bytes\n", p.len());
-                tcp_conn.send(std::move(p));
-                run();
-            });
-        }
-    };
-    tcp_test(ipv4& inet) : inet(inet), _listener(inet.get_tcp().listen(10000)) {}
-    void run() {
-        _listener.accept().then([this] (tcp::connection conn) {
-            (new connection(std::move(conn)))->run();
-            run();
-        });
-    }
-};
 
 int main(int ac, char** av) {
-    boost::program_options::variables_map opts;
-    opts.insert(std::make_pair("tap-device", boost::program_options::variable_value(std::string("tap0"), false)));
-    //
-    auto vnet = create_virtio_net_device(opts);
-    interface netif(std::move(vnet));
-    // ipv4 inet(&netif);
-    // inet.set_host_address(ipv4_address("192.168.122.2"));
-    // tcp_test tt(inet);
-    // engine().when_started().then([&tt] { tt.run(); });
-    // engine().run();
+    app_template app;
+    app.add_options()
+                    ("port", bpo::value<uint16_t>()->default_value(10000), "Server port")
+                    ("address", bpo::value<std::string>()->default_value("127.0.0.1"), "Server address")
+                    ("cert,c", bpo::value<std::string>()->required(), "Server certificate file")
+                    ("key,k", bpo::value<std::string>()->required(), "Certificate key")
+                    ("verbose,v", bpo::value<bool>()->default_value(false)->implicit_value(true), "Verbose")
+                    ;
+    return app.run_deprecated(ac, av, [&] {
+        auto&& config = app.configuration();
+        uint16_t port = config["port"].as<uint16_t>();
+        auto crt = config["cert"].as<std::string>();
+        auto key = config["key"].as<std::string>();
+        auto addr = config["address"].as<std::string>();
+        auto verbose = config["verbose"].as<bool>();
+
+        std::cout << "Starting..." << std::endl;
+        return net::dns::resolve_name(addr).then([=](net::inet_address a) {
+            ipv4_addr ia(a, port);
+
+            auto server = ::make_shared<seastar::sharded<echoserver>>();
+            return server->start(verbose).then([=]() {
+                return server->invoke_on_all(&echoserver::listen, socket_address(ia), sstring(crt), sstring(key), tls::client_auth::NONE);
+            }).handle_exception([=](auto e) {
+                std::cerr << "Error: " << e << std::endl;
+                engine().exit(1);
+            }).then([=] {
+                std::cout << "TLS echo server running at " << addr << ":" << port << std::endl;
+                engine().at_exit([server] {
+                    return server->stop();
+                });
+            });
+        });
+    });
 }
