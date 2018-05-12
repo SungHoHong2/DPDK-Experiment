6ec213b8055740b1c89f73a7302ee154f4b38bf7
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-12 12:25:38 -0700

    updated for DPDK-Experiments

--- dpdk-mpi/seastar-sendrecv/tcp_test.cc
+++ dpdk-mpi/seastar-sendrecv/tcp_test.cc
@@ -6,6 +6,7 @@
 using namespace seastar;
 using namespace net;
 using namespace std::chrono_literals;
+bool connect_failure = false;
 
 time_t start; //adding timer
 size_t BUFFER_SIZE = 64;
@@ -99,12 +100,11 @@ public:
         _total_pings = _pings_per_connection * _concurrent_connections;
         _test = test;
 
-        bool connect_failure = false;
         for (unsigned i = 0; i < ncon; i++) {
             socket_address local = socket_address(::sockaddr_in{AF_INET, INADDR_ANY, {0}});
-            engine().net().connect(make_ipv4_address(server_addr), local, protocol).then([this, test, connect_failure] (connected_socket fd) {
+            engine().net().connect(make_ipv4_address(server_addr), local, protocol).then([this, test] (connected_socket fd) {
                 auto conn = new connection(std::move(fd));
-                (this->*tests.at(test))(conn).then_wrapped([conn, connect_failure] (auto&& f) {
+                (this->*tests.at(test))(conn).then_wrapped([conn] (auto&& f) {
                     delete conn;
                     try {
                         f.get();
