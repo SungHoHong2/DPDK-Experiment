b80ee1eff5836abce39499309506ecea967ccc74
홍성호 <sunghohong@hongseoghouimbp.cidse.dhcp.asu.edu>
2018-05-12 16:57:53 -0700

    updated for DPDK-Experiments

--- dpdk-mpi/seastar-sendrecv/tcp_test.cc
+++ dpdk-mpi/seastar-sendrecv/tcp_test.cc
@@ -15,6 +15,8 @@
 using namespace seastar;
 using namespace net;
 using namespace std::chrono_literals;
+using namespace boost::interprocess;
+
 size_t BUFFER_SIZE = 64;
 
 #include "server_mpi.hh"
@@ -22,6 +24,30 @@ size_t BUFFER_SIZE = 64;
 namespace bpo = boost::program_options;
 
 int main(int ac, char ** av) {
+
+    //Remove shared memory on construction and destruction
+    struct shm_remove
+    {
+       shm_remove() {  shared_memory_object::remove("MySharedMemory"); }
+       ~shm_remove(){  shared_memory_object::remove("MySharedMemory"); }
+    } remover;
+
+    managed_shared_memory segment(create_only, "MySharedMemory", 65536);
+
+    //Allocate a portion of the segment (raw memory)
+    std::size_t free_memory = segment.get_free_memory();
+    void * shptr = segment.allocate(1024/*bytes to allocate*/);
+
+    //Check invariant
+    if(free_memory <= segment.get_free_memory())
+       return 1;
+
+    managed_shared_memory::handle_t handle = segment.get_handle_from_address(shptr);
+
+
+
+
+
     app_template app;
     app.add_options()
         ("server", bpo::value<std::string>()->default_value("10.218.111.252:1234"), "Server address")
