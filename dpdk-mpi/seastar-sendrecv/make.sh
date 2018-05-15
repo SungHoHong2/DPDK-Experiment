6889d3f23ae1fc056eb182e8847acc32d4648a22
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-05-15 23:56:02 +0900

    updated for DPDK-Experiments

--- dpdk-mpi/seastar-sendrecv/make.sh
+++ dpdk-mpi/seastar-sendrecv/make.sh
@@ -6,3 +6,6 @@ sudo gcc daemon-interface.c -o daemon-interface
 
 echo ./seastar-daemon --server "10.218.111.252:1234" --smp 1
 echo ./seastar-daemon --server "10.218.104.170:1234" --smp 1
+echo "\n"
+echo ./seastar-daemon  --dpdk-pmd --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.253 --netmask-ipv4-addr 255.255.248.0 --gw-ipv4-addr 10.218.111.1 --collectd 0 --smp 1
+echo ./seastar-daemon  --dpdk-pmd --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.254 --netmask-ipv4-addr 255.255.248.0 --gw-ipv4-addr 10.218.111.1 --collectd 0 --smp 1
