806434a8d906aca70a0bd643de67d1d04cdc85e7
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-05-15 09:05:37 +0900

    updated for DPDK-Experiments

--- dpdk-mpi/seastar-sendrecv/make.sh
+++ dpdk-mpi/seastar-sendrecv/make.sh
@@ -2,5 +2,7 @@ sudo git pull origin master
 wait
 sudo make
 wait
+gcc daemon-interface.c -o daemon-interface
+
 echo ./tcp_test --smp 1 --server "10.218.111.252:1234"
 echo ./tcp_test --smp 1 --server "10.218.104.170:1234"
