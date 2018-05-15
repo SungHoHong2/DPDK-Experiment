96df3cfd083fe9de550ba5feef972568c26fd5f7
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-05-15 09:43:17 +0900

    updated for DPDK-Experiments

--- dpdk-mpi/seastar-sendrecv/make.sh
+++ dpdk-mpi/seastar-sendrecv/make.sh
@@ -4,5 +4,5 @@ sudo make
 wait
 sudo gcc daemon-interface.c -o daemon-interface
 
-echo ./tcp_test --smp 1 --server "10.218.111.252:1234"
-echo ./tcp_test --smp 1 --server "10.218.104.170:1234"
+echo ./seastar-daemon --smp 1 --server "10.218.111.252:1234"
+echo ./seastar-daemon --smp 1 --server "10.218.104.170:1234"
