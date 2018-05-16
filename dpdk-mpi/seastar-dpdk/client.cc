4e7acd8b3d679272773e116a83e6d54b50bf8b9a
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-05-16 17:59:41 +0900

    updated for DPDK-Experiments

--- dpdk-mpi/seastar-dpdk/client.cc
+++ dpdk-mpi/seastar-dpdk/client.cc
@@ -65,7 +65,7 @@ public:
             return _write_buf.write(str).then([this] {
                 return _write_buf.flush();
             }).then([this, times] {
-                return _read_buf.read_exactly(4).then([this, times] (temporary_buffer<char> buf) {
+                return _read_buf.read().then([this, times] (temporary_buffer<char> buf) {
                     if (buf.size() != 4) {
                         fprint(std::cerr, "illegal packet received: %d\n", buf.size());
                         return make_ready_future();
--- dpdk-mpi/seastar-dpdk/make.sh
+++ dpdk-mpi/seastar-dpdk/make.sh
@@ -5,8 +5,6 @@ sudo make
 # sudo ./seastar-daemon  --dpdk-pmd  --dpdk-port-index 1  --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.253  --server "10.218.111.254:1234" --smp 1
 # sudo ./seastar-daemon  --dpdk-pmd  --dpdk-port-index 1  --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.254  --server "10.218.111.253:1234" --smp 1
 
-# sudo ./tcp_test --dpdk-pmd  --dpdk-port-index 1  --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.253  --server "10.218.111.254:1234" --smp 1
-# sudo ./tcp_test  --dpdk-pmd  --dpdk-port-index 1  --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.254  --server "10.218.111.253:1234" --smp 1
 
-
-# sudo ./seawreck  --dpdk-pmd  --dpdk-port-index 1  --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.253  --server "10.218.111.254:1234" --smp 1
+# sudo ./client --dpdk-pmd  --dpdk-port-index 1  --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.253  --server "10.218.111.254:1234"
+# sudo ./server  --dpdk-pmd  --dpdk-port-index 1  --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.254  --smp 1
