04456521bcfd2b143b602d763e06e0059be0f7ce
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-05-16 18:21:58 +0900

    updated for DPDK-Experiments

--- dpdk-mpi/seastar-dpdk/make.sh
+++ dpdk-mpi/seastar-dpdk/make.sh
@@ -1,6 +1,7 @@
 sudo git pull origin master
 wait
 sudo make
+sudo gcc daemon-interface.c -o daemon-interface
 
 # sudo ./seastar-daemon  --dpdk-pmd  --dpdk-port-index 1  --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.253  --server "10.218.111.254:1234" --smp 1
 # sudo ./seastar-daemon  --dpdk-pmd  --dpdk-port-index 1  --network-stack native --dhcp 0 --host-ipv4-addr 10.218.111.254  --server "10.218.111.253:1234" --smp 1
