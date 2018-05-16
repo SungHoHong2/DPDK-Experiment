9c2e8f023f1cc74b1f1778f9f32f875ec6f234b9
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-05-16 16:55:28 +0900

    updated for DPDK-Experiments

--- dpdk-mpi/seastar-sendrecv/daemon-interface.c
+++ dpdk-mpi/seastar-sendrecv/daemon-interface.c
@@ -34,10 +34,10 @@ int main() {
     else{
         pShardStuff = (struct shared_use_st *) pShardMemory;
         // cout << "[Client]shmat success. flag:" << pShardStuff->written_by_you << endl;;
-        int test_case[] = {
-          2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096
+        // int test_case[] = {
+          // 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096
           // 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304
-        };
+        // };
 
         int s =0;
         while(running){
@@ -46,15 +46,15 @@ int main() {
                 sleep(1);
             }
 
-            // printf("[Client]Enter text :\n");
-            // fgets(buffer, TEXT_SIZE, stdin);
+            printf("[Client]Enter text :\n");
+            fgets(buffer, TEXT_SIZE, stdin);
 
-            printf("starting with %d", test_case[s]);
-            int i=0;
-            for(i=0; i<test_case[s]; i++){
-              buffer[i]='*';
-            }
-            buffer[i]='\0';
+            // printf("starting with %d", test_case[s]);
+            // int i=0;
+            // for(i=0; i<test_case[s]; i++){
+            //   buffer[i]='*';
+            // }
+            // buffer[i]='\0';
 
             strncpy(pShardStuff->data, buffer, TEXT_SIZE);
             s++;
