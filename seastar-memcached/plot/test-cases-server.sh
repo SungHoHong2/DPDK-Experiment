3da3dc6b3051e6579a515353fcbd6b40787049e6
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-05-25 15:39:58 +0900

    updated for DPDK-Experiments

--- seastar-memcached/plot/test-cases-server.sh
+++ seastar-memcached/plot/test-cases-server.sh
@@ -1,11 +1,9 @@
 
-echo "howdy" $2 $3 
+if [ "$1" = "traditional" ]
+then
+memcached -u sungho -m 12000 -p 11211 -t $2
 
-# if [ "$2" = "traditional" ]
-# then
-# memcached -u sungho -m 12000 -p 11211 -t $3
-#
-# elif [ "$2" = "seastar" ]
-# then
-# /data1/sungho/seastar/build/release/apps/memcached/memcached --dpdk-pmd --dpdk-port-index 1 --network-stack native --dhcp 0 --host-ipv4-addr 10.107.30.40 --netmask-ipv4-addr 255.255.254.0 --gw-ipv4-addr 10.107.30.1 --collectd 0 --smp $3
-# fi
+elif [ "$1" = "seastar" ]
+then
+/data1/sungho/seastar/build/release/apps/memcached/memcached --dpdk-pmd --dpdk-port-index 1 --network-stack native --dhcp 0 --host-ipv4-addr 10.107.30.40 --netmask-ipv4-addr 255.255.254.0 --gw-ipv4-addr 10.107.30.1 --collectd 0 --smp $2
+fi
