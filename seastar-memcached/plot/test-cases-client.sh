4f8a71ae01e928bd1bbe60e35a14092c8debbb92
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-05-25 15:54:42 +0900

    updated for DPDK-Experiments

--- seastar-memcached/plot/test-cases-client.sh
+++ seastar-memcached/plot/test-cases-client.sh
@@ -1,6 +1,2 @@
 cd /data1/sungho/libmemcached-1.0.18/clients
-for i in 1 2 4 8
-do
-   ./memaslap -s 10.107.30.33:11211 -T 1 -S 1s -t 10s | grep "Run time: 10.0s\|   Avg:"
-   wait
-done
+./memaslap -s 10.107.30.33:11211 -T $1 -S 1s -t 10s | grep "Run time: 10.0s\|   Avg:"
