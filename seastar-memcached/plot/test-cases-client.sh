ab6f32beaab1a01cb8e284126d396a7d6408c123
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-14 15:15:13 +0900

    updated for DPDK-Experiments

--- seastar-memcached/plot/test-cases-client.sh
+++ seastar-memcached/plot/test-cases-client.sh
@@ -7,12 +7,12 @@ cd /data1/sungho/libmemcached-1.0.18/clients
 if [ "$1" = "traditional" ]
 then
 echo "test"
-
-    # if [ "$3" = "avg" ] then
-    # ./memaslap -s 10.107.30.33:11211 -T $2 -S 1s -t 10s | grep "Run time: 10.0s\|   Avg:"
-    # else
-    # ./memaslap -s 10.107.30.33:11211 -T $2 -S 1s -t 10s
-    # fi
+    if [ "$3" = "avg" ]
+    then
+    ./memaslap -s 10.107.30.33:11211 -T $2 -S 1s -t 10s | grep "Run time: 10.0s\|   Avg:"
+    else
+    ./memaslap -s 10.107.30.33:11211 -T $2 -S 1s -t 10s
+    fi
 
 
 elif [ "$1" = "seastar" ]
