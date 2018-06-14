0b97c9029d6d61766f38ec1e76ccecbee7e15402
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-14 17:15:40 +0900

    updated for DPDK-Experiments

--- seastar-memcached/seastar-version/install_memached.sh
+++ seastar-memcached/seastar-version/install_memached.sh
@@ -13,7 +13,7 @@ then
 rm -f *.o
    fi
 
-/data1/sungho/DPDK-Experiment/seastar-memcached/seastar-version/memcached --dpdk-pmd --dpdk-port-index 1 --network-stack native --dhcp 0 --host-ipv4-addr 10.107.30.40 --netmask-ipv4-addr 255.255.254.0 --gw-ipv4-addr 10.107.30.1 --collectd 0 --smp $2
+/data1/sungho/DPDK-Experiment/seastar-memcached/seastar-version/memcached --dpdk-pmd --dpdk-port-index 1 --network-stack native --dhcp 0 --host-ipv4-addr 10.107.30.40 --netmask-ipv4-addr 255.255.254.0 --gw-ipv4-addr 10.107.30.1 --collectd 0 --smp 1
 
 
 elif [ "$HOST_NAME" = "c3n25" ]
