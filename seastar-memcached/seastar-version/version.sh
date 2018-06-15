914997d8eb6ade10e67f6285b4d14e52f5df4e38
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-15 19:08:06 +0900

    memaslap experiments

--- seastar-memcached/seastar-version/version.sh
+++ seastar-memcached/seastar-version/version.sh
@@ -8,15 +8,22 @@ elif [ "$1" = "memcached_recover" ]
 then
 cp history/$FILENAME.cc memcached.cc
 
-elif [ "$1" = "memaslap_update" ]
-then
-mkdir history/$FILENAME
-cp memaslap/io.cc history/$FILENAME/io.cc
-cp memaslap/memaslap.c history/$FILENAME/memaslap.c
-
 elif [ "$1" = "memaslap_recover" ]
 then
-cp history/$FILENAME/io.cc io.cc
-cp history/$FILENAME/memaslap.c memaslap.c
+cp history/$FILENAME/io.cc memaslap/io.cc
+cp history/$FILENAME/memaslap.c memaslap/memaslap.c
+
+elif [ "$1" = "memaslap_import" ]
+then
+cp /Users/sunghohong/Documents/Projects/libmemcached-1.0.18/libmemcached/io.cc memaslap/io.cc
+cp /Users/sunghohong/Documents/Projects/libmemcached-1.0.18/clients/memaslap.c memaslap/memaslap.c
+
+mkdir history/$FILENAME
+cp /Users/sunghohong/Documents/Projects/libmemcached-1.0.18/libmemcached/io.cc history/$FILENAME/io.cc
+cp /Users/sunghohong/Documents/Projects/libmemcached-1.0.18/clients/memaslap.c history/$FILENAME/memaslap.c
+
+git add .
+git commit -m 'memaslap experiments'
+git push origin master
 
 fi
