3008b34c3fae75293eb5db79ef5e9a4b564f3db4
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-16 20:04:04 +0900

    updated for DPDK-Experiments

--- seastar-memcached/seastar-version/tcp-client/client.cc
+++ seastar-memcached/seastar-version/tcp-client/client.cc
@@ -30,6 +30,26 @@ int main(){
       fprintf(stderr, "Couldn't add server: %s\n", memcached_strerror(memc, rc));
 
 
+    rc = memcached_set(memc, key, strlen(key), value, strlen(value), (time_t)0, (uint32_t)0);
+
+
+    if (rc == MEMCACHED_SUCCESS)
+      fprintf(stderr, "Key stored successfully\n");
+    else
+      fprintf(stderr, "Couldn't store key: %s\n", memcached_strerror(memc, rc));
+
+    retrieved_value = memcached_get(memc, key, strlen(key), &value_length, &flags, &rc);
+
+
+    if (rc == MEMCACHED_SUCCESS) {
+      fprintf(stderr, "Key retrieved successfully\n");
+      printf("The key '%s' returned value '%s'.\n", key, retrieved_value);
+      free(retrieved_value);
+    }
+    else
+      fprintf(stderr, "Couldn't retrieve key: %s\n", memcached_strerror(memc, rc));
+
+
     std::cout << "memcached END" << std::endl;
     return 0;
 }
