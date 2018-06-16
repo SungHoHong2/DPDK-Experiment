8cb1efd00c6a9d36a8bd0d50cec870cb722c4dcc
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-16 22:02:42 +0900

    updated for DPDK-Experiments

--- seastar-memcached/seastar-version/tcp-client/dpdk_memcached.hh
+++ seastar-memcached/seastar-version/tcp-client/dpdk_memcached.hh
@@ -6,6 +6,8 @@
 #include <string>
 #include <algorithm>
 
-int dpdk_memcached_set();
-
+memcached_return_t memcached_set(memcached_st *ptr, const char *key, size_t key_length,
+                                 const char *value, size_t value_length,
+                                 time_t expiration,
+                                 uint32_t flags);
 #endif
