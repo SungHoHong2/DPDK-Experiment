99ef75655090a233dc9324f10fdb7acf2085c75b
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-16 22:19:18 +0900

    updated for DPDK-Experiments

--- seastar-memcached/seastar-version/tcp-client/dpdk_memcached.cc
+++ seastar-memcached/seastar-version/tcp-client/dpdk_memcached.cc
@@ -1,6 +1,14 @@
 #include "dpdk_memcached.hh"
 
 
+
+
+
+
+
+
+
+
 memcached_return_t dpdk_memcached_set(memcached_st *ptr, const char *key, size_t key_length,
                                  const char *value, size_t value_length,
                                  time_t expiration,
@@ -8,11 +16,11 @@ memcached_return_t dpdk_memcached_set(memcached_st *ptr, const char *key, size_t
 {
     std::cout << "dpdk_memcached_set BEGIN" << std::endl;
     memcached_return_t rc;
-//    LIBMEMCACHED_MEMCACHED_SET_START();
+    LIBMEMCACHED_MEMCACHED_SET_START();
 //    rc= memcached_send(ptr, key, key_length,
 //                       key, key_length, value, value_length,
 //                       expiration, flags, 0, SET_OP);
-//    LIBMEMCACHED_MEMCACHED_SET_END();
+    LIBMEMCACHED_MEMCACHED_SET_END();
 
     std::cout << "dpdk_memcached_set END" << std::endl;
     return rc;
