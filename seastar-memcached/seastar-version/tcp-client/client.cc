d13f23abc3dc3c62cb80be2b547a85b0895818c4
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-16 19:47:39 +0900

    updated for DPDK-Experiments

--- seastar-memcached/seastar-version/tcp-client/client.cc
+++ seastar-memcached/seastar-version/tcp-client/client.cc
@@ -1,12 +1,35 @@
 #include <libmemcached/memcached.hpp>
-#include <stdio.h>
-#include <string.h>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+
 
 int main(){
-  std::cout << "Hello" << std::endl;
+    std::cout << "memcached BEGIN" << std::endl;
 
 
+    memcached_server_st *servers = NULL;
+    memcached_st *memc;
+    memcached_return rc;
+    char *key = "keystring";
+    char *value = "keyvalue";
 
-  return 0;
+    char *retrieved_value;
+    size_t value_length;
+    uint32_t flags;
+
+    memc = memcached_create(NULL);
+    servers = memcached_server_list_append(servers, "10.107.30.40", 11211, &rc);
+    rc = memcached_server_push(memc, servers);
+
+    if (rc == MEMCACHED_SUCCESS)
+      fprintf(stderr, "Added server successfully\n");
+    else
+      fprintf(stderr, "Couldn't add server: %s\n", memcached_strerror(memc, rc));
+
+
+    std::cout << "memcached END" << std::endl;
+    return 0;
 }
