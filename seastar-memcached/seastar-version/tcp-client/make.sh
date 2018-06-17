64bb905ea387cb8889cf5a2c301495f347080ff8
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-17 15:27:30 +0900

    updated for DPDK-Experiments

--- seastar-memcached/seastar-version/tcp-client/make.sh
+++ seastar-memcached/seastar-version/tcp-client/make.sh
@@ -1,8 +1,7 @@
 git pull origin master
 cd /data1/sungho/DPDK-Experiment/seastar-memcached/seastar-version/tcp-client
-SOURCE_FILES="client.cc dpdk_memcached.cc "
-SOURCE_FILES+="rijndael.cc "
-SOURCE_FILES+="dpdk_string.cc "
+SOURCE_FILES="client.cc dpdk_memcached.cc rijndael.cc dpdk_string.cc "
+
 
 export PATH=~/data1/sungho/usr/bin:$PATH
 
