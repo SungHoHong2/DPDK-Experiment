063d0a3f79481e3e0a30a4fd506c17ee52976f75
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-18 16:11:44 +0900

    memaslap experiments

--- seastar-memcached/seastar-version/history/8_memaslap/memaslap/make.sh
+++ seastar-memcached/seastar-version/history/8_memaslap/memaslap/make.sh
@@ -4,5 +4,4 @@ SOURCE_FILES="memaslap.c ms_conn.c ms_setting.c ms_sigsegv.c ms_stats.c ms_task.
 
 
 # export PATH=~/data1/sungho/usr/bin:$PATH
-
 gcc -g -O2 -lm $SOURCE_FILES -o client -lpthread  -levent -lmemcached -lmemcachedutil
