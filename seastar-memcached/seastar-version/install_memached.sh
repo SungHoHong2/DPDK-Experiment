4a4483ff5eb0f0dd6893c1f12adf746f7f04c30d
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-18 17:26:30 +0900

    memaslap experiments

--- seastar-memcached/seastar-version/install_memached.sh
+++ seastar-memcached/seastar-version/install_memached.sh
@@ -64,7 +64,7 @@ cp /data1/sungho/DPDK-Experiment/seastar-memcached/seastar-version/memaslap/ms_c
 cd /data1/sungho/libmemcached-1.0.18/
 # /data1/sungho/libmemcached-1.0.18/configure --enable-memaslap LDFLAGS=-lpthread --prefix=/data1/sungho/usr/local
 # make -j 16 -n && sudo make install -j 16
-make -j 16 && sudo make install -j 16
+make -j 16 -n && sudo make install -j 16 -n
 
   else
 cd /data1/sungho/libmemcached-1.0.18/clients
