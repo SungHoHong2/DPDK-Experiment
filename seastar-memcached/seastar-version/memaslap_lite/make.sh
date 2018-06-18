876b93babaef6e535330a994828b9f5663bef170
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-18 23:18:46 +0900

    memaslap_lite experiments

--- seastar-memcached/seastar-version/memaslap_lite/make.sh
+++ seastar-memcached/seastar-version/memaslap_lite/make.sh
@@ -9,7 +9,7 @@ elif [ "$1" = "memaslap_cpp" ]
 then
 depbase=`echo clients/memaslap.o | sed 's|[^/]*$|.deps/&|;s|\.o$||'`
 echo $depbase
-c++ -std=c++0x  -DHAVE_CONFIG_H -I. -fvisibility=hidden  -g -O2  -Wno-unknown-pragmas -Wno-pragmas -Wall -Wextra -Wunsuffixed-float-constants -Wjump-misses-init -Wno-attributes -Waddress -Wvarargs -Warray-bounds -Wbad-function-cast -Wchar-subscripts -Wcomment -Wfloat-equal -Wformat-security -Wformat=2 -Wformat-y2k -Wlogical-op -Wmaybe-uninitialized -Wmissing-field-initializers -Wmissing-noreturn -Wmissing-prototypes -Wnested-externs -Wnormalized=id -Woverride-init -Wpointer-arith -Wpointer-sign -Wredundant-decls -Wshadow -Wsign-compare -Wstrict-overflow=1 -Wswitch-enum -Wtrampolines -Wundef -Wunsafe-loop-optimizations -funsafe-loop-optimizations -Wclobbered -Wunused -Wunused-result -Wunused-variable -Wunused-parameter -Wunused-local-typedefs -Wwrite-strings -fwrapv -pipe -fPIE -pie -Wsizeof-pointer-memaccess -Wpacked -MT clients/memaslap.o -MD -MP -MF $depbase.Tpo -c -o clients/memaslap.o clients/memaslap.cc
+c++ -std=c++0x  -DHAVE_CONFIG_H -I. -fvisibility=hidden  -g -O2 -MT clients/memaslap.o -MD -MP -MF $depbase.Tpo -c -o clients/memaslap.o clients/memaslap.cc
 mv -f $depbase.Tpo $depbase.Po
 
 elif [ "$1" = "ms_conn" ]
