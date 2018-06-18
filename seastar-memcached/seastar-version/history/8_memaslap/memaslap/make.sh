b3e8bb4cec09349a15729470789945fb8da33a6d
홍성호 <sunghohong@hongseonghoui-MacBook-Pro.local>
2018-06-18 15:38:27 +0900

    memaslap experiments

--- seastar-memcached/seastar-version/history/8_memaslap/memaslap/make.sh
+++ seastar-memcached/seastar-version/history/8_memaslap/memaslap/make.sh
@@ -1,4 +1,4 @@
-git pull origin master
+# git pull origin master
 # cd /data1/sungho/DPDK-Experiment/seastar-memcached/seastar-version/tcp-client
 SOURCE_FILES="memaslap.c ms_conn.c ms_setting.c ms_sigsegv.c ms_stats.c ms_task.c ms_thread.c "
 
